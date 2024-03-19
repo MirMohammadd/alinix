73f4dd16c8ea17374c86de3a130c1cef9592a96d
Heisenberg <alimirmohammad.1386@gmail.com>
2024-03-19 19:10:30 -0400

    Updated keyboard characters

--- src/keyboard.c
+++ src/keyboard.c
@@ -30,8 +30,38 @@ static int get_scancode(){
         return scancode;
     }
     return 0;
-    
 
 
 
+
+}
+
+
+char alternate_chars(char ch){
+    switch(ch){
+        case '`':return '~';
+        case '1': return '!';
+        case '2': return "@";
+        case '3': return '#';
+        case '4': return '$';
+        case '5': return  '%';
+        case '6': return '^';
+        case '7': return  '&';
+        case  '8': return   '*';
+        case '9' : return   '(';
+        case '0':return ')';
+        case '-': return '_';
+        case '=': return '+';
+        case '[': return '{';
+        case ']': return '}';
+        case '\\': return '|';
+        case ';': return ':';
+        case '\'': return '\"';
+        case ',': return '<';
+        case '.': return '>';
+        case '/': return '?';
+        default: return ch;
+
+
+    }
 }

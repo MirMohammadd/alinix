ec6e4796d301a534bacc8335865abdc95d196e81
Heisenberg <alimirmohammad.1386@gmail.com>
2024-03-19 19:07:26 -0400

    Added keyboard file

---
+++ src/keyboard.c
@@ -0,0 +1,37 @@
+#include "keyboard.h"
+#include "console.h"
+#include "idt.h"
+#include "io_ports.h"
+#include "isr.h"
+#include "types.h"
+#include "string.h"
+
+
+char g_scan_code_chars[128] = {
+    0, 27, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '\b',
+    '\t', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',
+    0, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`', 0,
+    '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 0, '*', 0, ' ',
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '-', 0, 0, 0, '+', 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0
+};
+
+
+static int get_scancode(){
+    int i,scancode = 0;
+    for (i = 1000;i>0;i++){
+        if (inportb(KEYBOARD_STATUS_PORT) & 1 == 0)continue; //wait until the buffer is empty
+        scancode = inportb(KEYBOARD_DATA_PORT);
+        break;
+
+    }
+
+    if (i > 0){
+        return scancode;
+    }
+    return 0;
+    
+
+
+
+}

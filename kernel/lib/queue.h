71ff23f8eb74cd9e1633b2a676955c80e16bd950
Heisenberg <alimirmohammad.1386@gmail.com>
2024-03-20 17:32:03 -0400

    Added license

--- kernel/lib/queue.h
+++ kernel/lib/queue.h
@@ -1,3 +1,18 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2024 Heisenberg
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * @author Heisenberg
+
+ */
 /*
  * Copyright (c) 1991, 1993
  *	The Regents of the University of California.  All rights reserved.

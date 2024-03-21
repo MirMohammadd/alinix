69093f36f9e553aaebe5823d3d994b2ca472d4b1
Heisenberg <alimirmohammad.1386@gmail.com>
2024-03-21 17:00:05 -0400

    Useful macros

---
+++ kernel/hdr/ahci.h
@@ -0,0 +1,32 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2024 Heisenberg
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * @author Heisenberg
+
+ */
+
+
+#ifndef __AHCI_H__
+#define __AHCI_H__
+
+#include <stdint.h>
+
+/*Defining some macro for ports*/
+
+#define PX_CMD_START   1
+#define PX_CMD_POD 2
+#define PX_CMD_SUD 4
+#define PX_CMD_FRE (1<<4)
+#define PX_CMD_FR (1<<14)
+
+
+#endif
---
+++ kernel/hdr/aurora.h
@@ -0,0 +1,106 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2024 Heisenberg
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * @author Heisenberg
+
+ */
+
+#ifndef __AURORA_H__
+#define __AURORA_H__
+
+#include <stdint.h>
+
+#define AU_EXPORT __declspec(dllexport)
+#define AU_IMPORT  __declspec(dllimport)
+#ifdef __AU_KERNEL__
+#define AU_FUNC AU_EXPORT
+#else
+#define AU_FUNC AU_IMPORT
+#endif
+
+#define AU_EXTERN  extern "C"
+
+
+#define KERNEL_STACK_LOCATION   0xFFFFFB0000000000
+
+
+/**
+* Kernel Boot information structure passed by XNLDR
+*/
+#pragma pack (push,1)
+typedef struct _KERNEL_BOOT_INFO_ {
+	/* Boot type either UEFI_BOOT or BIOS_BOOT */
+	int boot_type;
+
+	void* allocated_stack;
+
+	uint64_t reserved_mem_count;
+
+	/* map -- UEFI memory map */
+	void *map;
+
+	/* descriptor_size -- UEFI memory map descriptor size */
+	uint64_t descriptor_size;
+
+	/* mem_map_size -- UEFI memory map size */
+	uint64_t mem_map_size;
+
+	/* graphics_framebuffer -- framebuffer address passed by XNLDR */
+	uint32_t* graphics_framebuffer;
+
+	/* fb_size -- framebuffer total size */
+	size_t   fb_size;
+
+	/* X_Resolution -- Total width of the entire display */
+	uint16_t  X_Resolution;
+
+	/* Y_Resolution -- Total height of the entire display */
+	uint16_t   Y_Resolution;
+
+	/* pixels_per_line -- scanline of the current display */
+	uint16_t   pixels_per_line;
+
+	/* redmask, greenmask, bluemask, resvmask -- color mask */
+	uint32_t redmask;
+	uint32_t greenmask;
+	uint32_t bluemask;
+	uint32_t resvmask;
+
+	/* acpi_table_pointer -- acpi base memory map */
+	void*    acpi_table_pointer;
+
+	/* kernel_size -- total kernel image size */
+	size_t   kernel_size;
+
+	/* psf_font_data -- screen font address loaded
+	by XNLDR to use for debugging purpose */
+	uint8_t* psf_font_data;
+
+	/* printf_gui -- character printing function pointer to use
+	for debugging purpose provided by XNLDR */
+	void(*printf_gui) (const char* text, ...);
+
+	/* unused pointer entries */
+	uint8_t* driver_entry1;   //OTHER
+	uint8_t *driver_entry2;   //!NVME
+	uint8_t *driver_entry3;   //!AHCI
+	uint8_t* driver_entry4;   //!FLOPPY
+	uint8_t* driver_entry5;   //!ATA
+	uint8_t* driver_entry6;   //!USB
+	void*    apcode;
+	uint32_t hid;
+	uint32_t uid;
+	uint32_t cid;
+}KERNEL_BOOT_INFO, *PKERNEL_BOOT_INFO;
+#pragma pack(pop)
+
+#endif

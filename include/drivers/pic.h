1c4f01f1931f65adadab60fa61f599f2b9e6fbe5
Heisenberg <alimirmohammad.1386@gmail.com>
2024-03-21 18:04:11 -0400

    Some edits regarding #35

--- include/drivers/pic.h
+++ include/drivers/pic.h
@@ -11,38 +11,7 @@
  * furnished to do so, subject to the following conditions:
  *
  * @author Heisenberg
- * @file mouse.h
+ * @file pic.h
 
  */
 
-
-#ifndef MOUSE_H
-#define MOUSE_H
-
-#include <types.h>
-
-
-#define MOUSE_PORT 0x60
-#define MOUSE_STATUS  0x80
-#define MOUSE_ABIT 0x02
-#define MOUSE_BBIT 0x01
-#define MOUSE_WRITE  0xD4
-#define MOUSE_F_BIT  0x20
-#define MOUSE_V_BIT  0x08
-
-
-/*Mouse clocks defined here*/
-
-
-#define LEFT_CLICK      0x1
-#define RIGHT_CLICK     0x2
-#define MIDDLE_CLICK    0x4
-
-typedef struct mouse_info{
-    int x;
-    int y;
-    uint32_t buttons;  
-}mouse_info_t;
-
-
-#endif // MOUSE_H

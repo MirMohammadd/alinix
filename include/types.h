fe728d8ce2a28c234ef7abb3acc4493e3aaea21b
Heisenberg <alimirmohammad.1386@gmail.com>
2024-03-21 17:17:15 -0400

    Added more drivers regarding #35

--- include/types.h
+++ include/types.h
@@ -1,44 +1,21 @@
-/*
- * Copyright (c) 2024 Heisenberg. All rights reserved.
+/**
+ * MIT License
+ *
+ * Copyright (c) 2024 Heisenberg
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO, THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ *
+ * @author Heisenberg
+
  */
-#ifndef TYPES_H
-#define TYPES_H
+#ifndef IO_H
+#define IO_H
 
-#define NULL 0
-
-typedef unsigned char uint8;
-typedef unsigned short uint16;
-typedef unsigned int uint32;
-typedef signed char sint8;
-typedef signed short sint16;
-typedef signed int sint32;
-typedef uint8 byte;
-typedef uint16 word;
-typedef uint32 dword;
-typedef unsigned long long uint64;
-typedef long long int64;
-typedef int64 qword;
-typedef uint32 size_t; 
-typedef uint32 fd_t;
-typedef enum {
-    FALSE,
-    TRUE
-} BOOL;
 #define NULL 0
 
 typedef unsigned char uint8_t;
@@ -76,5 +53,4 @@ typedef __builtin_va_list va_list;
 #define va_end(ap) __builtin_va_end(ap)
 #define va_arg(ap,type) __builtin_va_arg(ap,type)
 
-#endif // TYPES_H
-
+#endif

9a9ccb8275a5e886ee08669a1ded7fb69f9ad6b5
Heisenberg <alimirmohammad.1386@gmail.com>
2024-03-23 19:25:11 -0400

    Patch

---
+++ arch/arm/include/asm/cpu.h
@@ -0,0 +1,22 @@
+/*
+ *  arch/arm/include/asm/cpu.h
+ *
+ *  Copyright (C) 2004-2005 ARM Ltd.
+ */
+#ifndef __ASM_ARM_CPU_H
+#define __ASM_ARM_CPU_H
+
+#include <heisen/percpu.h>
+#include <heisen/cpu.h>
+
+struct cpuinfo_arm {
+	struct cpu	cpu;
+	u32		cpuid;
+#ifdef CONFIG_SMP
+	unsigned int	loops_per_jiffy;
+#endif
+};
+
+DECLARE_PER_CPU(struct cpuinfo_arm, cpu_data);
+
+#endif

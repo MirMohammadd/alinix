72d8e5a37df5bbaf46f31e7e656264c8067381b3
Heisenberg <alimirmohammad.1386@gmail.com>
2024-03-31 17:43:51 -0400

    Minor update

--- kernel/main.c
+++ kernel/main.c
@@ -10,6 +10,13 @@
 #include <proc/sched.h>
 #include "graphics.h"
 #include "terminal.h"
+#include "yutani.h"
+
+static yutani_t * yctx;
+static yutani_window_t * window = NULL;
+static char * title_str;
+
+
 int kmain(multiboot_info_t *info) {
     
     video_init(25, 80);
@@ -34,6 +41,9 @@ int kmain(multiboot_info_t *info) {
     ata_init();
     
     sched_init();
+
+
+	yutani_window_advertise_icon(yctx, window, title_str, "star");
 
 
     

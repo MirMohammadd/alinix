2eb72e0805c6c990488a343744c70f76378653ad
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-01 15:02:08 -0400

    Update

--- kernel/src/main.cc
+++ kernel/src/main.cc
@@ -9,3 +9,39 @@
 #include <string.hpp>
 #include <log.hpp>
 #include <proc.hpp>
+
+
+using namespace LIBHeisenKernel;
+
+Window* mainWindow = 0;
+
+void ValueChanged(void* s, int v)
+{
+    mainWindow->backColor = v * 1000;
+}
+
+int main(int argc, char** argv)
+{
+    GUI::SetDefaultFont();
+
+    mainWindow = new Window(600, 600, 300, 300);
+    mainWindow->titleString = "CactusOS File Browser";
+
+    ScrollBar* scroll = new ScrollBar(Vertical);
+    scroll->x = 100;
+    scroll->y = 200;
+    scroll->value.onChanged += ValueChanged;
+    mainWindow->AddChild(scroll);
+
+    while (GUI::HasItems()) {
+        GUI::DrawGUI();
+        GUI::ProcessEvents();
+        //scroll->value += 1;
+        if(scroll->value >= scroll->maxValue) {
+            scroll->value = 0;
+            scroll->maxValue += 20;
+        }
+    }
+
+    return 0;
+}

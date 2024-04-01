0eacb10aad7acc513c32a01ee16dcfe5ddd9f0f7
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-01 15:22:52 -0400

    Patch

--- kernel/arch/x86-pc/startup.c
+++ kernel/arch/x86-pc/startup.c
@@ -21,9 +21,7 @@
 #include <io/console.h>
 #include <colorforth/colorforth.h>
 #include <arch/x86-pc/io/video.h>
-#include <gui/widgets/button.hpp>
-
-
+#include <kernel/include/gui/widgets/button.hpp>
 
 /**
  * The kernel entry point. All starts from here!

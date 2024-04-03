23f9d04656d059bde936d4ff7b077fb1f3c2867a
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-02 22:34:14 -0400

    Minor patch

--- kernelz/src/core/idthelper.s
+++ kernelz/src/core/idthelper.s
@@ -6,7 +6,7 @@ HandleException\num:
 
 .macro HandleExceptionNoError num
 HandleException\num:
-    pushl $0 ; Add empty error code
+    pushl $0           ; Add empty error code
     pushl $\num
     jmp interrupthandler
 .endm
@@ -18,31 +18,27 @@ HandleInterruptRequest\num:
     jmp interrupthandler
 .endm
 
-HandleExceptionNoError:
-HandleExceptionNoError 0x00
-HandleExceptionNoError 0x01
-HandleExceptionNoError 0x02
-HandleExceptionNoError 0x03
-HandleExceptionNoError 0x04
-HandleExceptionNoError 0x05
-HandleExceptionNoError 0x06
-HandleExceptionNoError 0x07
-HandleExceptionWithError:
+HandleExceptionWithError 0x00
+HandleExceptionWithError 0x01
+HandleExceptionWithError 0x02
+HandleExceptionWithError 0x03
+HandleExceptionWithError 0x04
+HandleExceptionWithError 0x05
+HandleExceptionWithError 0x06
+HandleExceptionWithError 0x07
 HandleExceptionWithError 0x08
-HandleExceptionNoError 0x09
+HandleExceptionWithError 0x09
 HandleExceptionWithError 0x0A
 HandleExceptionWithError 0x0B
 HandleExceptionWithError 0x0C
 HandleExceptionWithError 0x0D
 HandleExceptionWithError 0x0E
-HandleExceptionNoError:
 HandleExceptionNoError 0x0F
 HandleExceptionNoError 0x10
 HandleExceptionNoError 0x11
 HandleExceptionNoError 0x12
 HandleExceptionNoError 0x13
 
-HandleInterruptRequest:
 HandleInterruptRequest 0x00
 HandleInterruptRequest 0x01
 HandleInterruptRequest 0x02
@@ -64,8 +60,8 @@ HandleInterruptRequest 0xDD
 HandleInterruptRequest 0x60
 
 interrupthandler:
-    cli ; Stop Interrupts
-    
+    cli                 ; Stop Interrupts
+
     ; Save Registers
     pusha
     pushl %ds
@@ -73,15 +69,14 @@ interrupthandler:
     pushl %fs
     pushl %gs
 
-    ; load the kernel data segment descriptor
+    ; Load the kernel data segment descriptor
     mov $0x10, %ax
     mov %ax, %ds
     mov %ax, %es
     mov %ax, %fs
     mov %ax, %gs
 
-    pushl %esp
-    ; Call the kernel IRQ handler
+    pushl %esp          ; Call the kernel IRQ handler
     call _ZN8CactusOS4core24InterruptDescriptorTable15HandleInterruptEPNS0_8CPUStateE
     mov %eax, %esp
 
@@ -95,7 +90,7 @@ interrupthandler:
     ; Clean up
     add $8, %esp
 
-    sti ; Restart Interrupts
+    sti                 ; Restart Interrupts
     iret
 
 .globl IgnoreInterrupt

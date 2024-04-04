434b60588bda03a17f8d151c0a1fb5e4ea4a87ac
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-03 20:53:12 -0400

    Minor update

--- kernelz/src/system/tasking/userspace.asm
+++ kernelz/src/system/tasking/userspace.asm
@@ -1,3 +1,7 @@
+section .text
+
+global enter_usermode
+
 enter_usermode:
     push rbp
     mov rbp, rsp
@@ -12,18 +16,18 @@ enter_usermode:
 
     push 0x23
 
-    mov rcx, [rbp + 8]
+    mov rcx, [rbp + 8]      
     mov rsp, rcx
 
     pushfq
     pop rax
 
-    or rax, [rbp + 16]
+    or rax, [rbp + 16]     
     push rax
 
     push 0x1B
 
-    mov rax, [rbp + 16]
+    mov rax, [rbp + 16]     
     push rax
 
     iretq

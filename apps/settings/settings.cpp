f99968ae1ac0e7c3cc9222ce0b245a4d15a53f5f
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-04 19:45:35 -0400

    Patch

--- apps/settings/settings.cpp
+++ apps/settings/settings.cpp
@@ -15,6 +15,7 @@ char* path = "B:\\settings.jpg";
 void CapacityDriveCallback(void* sender, MouseButtonArgs args){
     double size = getDriveSize(0);
     Print("Drive Size: %f\n", size);
+    
 }
 
 void SystemArgumentsCallBack(void* sender, MouseButtonArgs args){

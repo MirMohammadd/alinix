62e37f2979ac119522d4407c3c1467f2209588f5
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-05 18:23:51 -0400

    Patch for the usb drivers

---
+++ lib/include/xhci.h
@@ -0,0 +1,48 @@
+#ifndef _HEISEN_KERNEL_XHCI__
+#define _HEISEN_KERNEL_XHCI__
+
+#if defined(__cplusplus)
+#include "types.h"
+using namespace LIBHeisenKernel;
+
+#else 
+#include <stdint.h>
+
+#endif 
+
+
+
+#pragma pack(push,1)
+typedef struct _xhci_doorbell_ {
+	unsigned doorbell[256];
+}xhci_doorbell_regs_t;
+#pragma pack(pop)
+
+#pragma pack(push,1)
+typedef struct _xhci_cap_regs_ {
+	uint8_t cap_caplen_version;
+	uint8_t reserved;
+	uint16_t hci_version;
+	uint32_t cap_hcsparams1;
+	uint32_t cap_hcsparams2;
+	uint32_t cap_hcsparams3;
+	uint32_t cap_hccparams1;
+	uint32_t cap_dboff;
+	uint32_t cap_hccparams2;
+}xhci_cap_regs_t;
+#pragma pack(pop)
+
+#pragma pack(push,1)
+typedef struct _xhci_cap_regs_ {
+	uint8_t cap_caplen_version;
+	uint8_t reserved;
+	uint16_t hci_version;
+	uint32_t cap_hcsparams1;
+	uint32_t cap_hcsparams2;
+	uint32_t cap_hcsparams3;
+	uint32_t cap_hccparams1;
+	uint32_t cap_dboff;
+	uint32_t cap_hccparams2;
+}xhci_cap_regs_t;
+#pragma pack(pop)
+#endif

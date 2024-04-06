463050f9d5831694ca961ca7ab3474e3313d549f
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-06 12:28:34 -0400

    Minor update

--- kernelz/src/core/gdthelper.asm
+++ kernelz/src/core/gdthelper.asm
@@ -1,3 +1,4 @@
+section .text
 global gdt_flush
 
 gdt_flush:
@@ -10,6 +11,6 @@ gdt_flush:
    mov fs, ax
    mov gs, ax
    mov ss, ax
-   jmp 0x08:.flush   
+   jmp 0x08, .flush   
 .flush:
    ret

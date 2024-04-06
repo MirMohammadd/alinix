91b4999aeed7583bc62fbeef70931574960f20d4
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-06 00:22:57 -0400

    Minor update

--- lib/include/types.h
+++ lib/include/types.h
@@ -36,21 +36,21 @@ namespace LIBHeisenKernel
     }
 }
 
-extern "C"{
+// extern "C"{
 
-    typedef char                        int8_t;
-    typedef unsigned char               uint8_t;
-    typedef short                       int16_t;
-    typedef unsigned short              uint16_t;
-    typedef int                         int32_t;
-    typedef unsigned int                uint32_t;
-    typedef long long int               int64_t;
-    typedef unsigned long long int      uint64_t;
-    typedef unsigned long long          uintptr_t;
-    typedef uint32_t size_t; 
-    typedef unsigned char byte_t;
-    typedef unsigned int dword_t;
-    typedef long unsigned int word_t;
-}
+//     typedef char                        int8_t;
+//     typedef unsigned char               uint8_t;
+//     typedef short                       int16_t;
+//     typedef unsigned short              uint16_t;
+//     typedef int                         int32_t;
+//     typedef unsigned int                uint32_t;
+//     typedef long long int               int64_t;
+//     typedef unsigned long long int      uint64_t;
+//     typedef unsigned long long          uintptr_t;
+//     typedef uint32_t size_t; 
+//     typedef unsigned char byte_t;
+//     typedef unsigned int dword_t;
+//     typedef long unsigned int word_t;
+// }
 
 #endif

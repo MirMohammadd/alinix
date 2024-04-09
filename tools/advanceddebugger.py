3aa2decaed8dbe7f3e2389c4b8ca3f2287d7e5e6
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-08 20:24:40 -0400

    Patch

--- tools/advanceddebugger.py
+++ tools/advanceddebugger.py
@@ -57,6 +57,7 @@ class Debugger(unittest.TestCase,AttribClass):
 def parse_arguments(debug=False):
     parser = argparse.ArgumentParser(description='Advanced Debugger')
     parser.add_argument("--level",type=int,default=1,required=False,help="Debug level, range should be 1 to 6 (default: 1)")
+    parser.add_argument("-v","--verbose",action='store_true',required=False,help="Enable verbose output",default=1)
     parser.add_argument('-d', '--debug', action='store_true', help='Enable debug output')
     return parser.parse_args(
         

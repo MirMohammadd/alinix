660cac1dfc662adc7738664f37e24d7e24841f59
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-11 17:54:24 -0400

    Patch

---
+++ apps/chess/square.h
@@ -0,0 +1,17 @@
+#ifndef _HEISEN_SQUARE_LIB_APPS_CHESS_GAMES_H
+#define _HEISEN_SQUARE_LIB_APPS_CHESS_GAMES_H
+
+#include "colour.h"
+
+namespace LIBHeisenKernel{
+    namespace engine{
+        /**
+         * @brief Constants are defined here
+        */
+
+        const int FieldNb = 16;
+        const int RandNb = 16;
+    };
+};
+
+#endif // _HEISEN_SQUARE_LIB_APPS_CHESS_GAMES_H

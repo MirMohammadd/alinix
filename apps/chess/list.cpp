e411906acadb0fb6592ac75b58dc29d558048234
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-12 19:09:45 -0400

    Patch

---
+++ apps/chess/list.cpp
@@ -0,0 +1,24 @@
+#include "board.h"
+#include "list.hpp"
+#include "move.h"
+#include "util.h"
+#include "value.h"
+#include <types.h>
+
+using namespace LIBHeisenKernel;
+using namespace HeisenOs::common;
+
+namespace LIBHeisenKernel{
+    namespace engine{
+        /**
+         * @brief Defining the list structure here
+        */
+        static const bool UserStrict = true;
+
+
+        bool list_is_ok(const list_t * list){
+            if (list == NULL) return false;
+
+        }
+    }
+};

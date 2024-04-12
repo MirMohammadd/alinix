891f4f3e65d695e90fc47db87d85c254d08e69a8
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-12 18:10:42 -0400

    Patch

---
+++ apps/chess/recog.h
@@ -0,0 +1,13 @@
+#ifndef _ALINIX_KERNEL_CHESS_GAME_APP_RECOG_H
+#define _ALINIX_KERNEL_CHESS_GAME_APP_RECOG_H
+
+#include "board.h"
+#include "util.h"
+
+namespace LIBHeisenKernel{
+    namespace engine{
+        extern bool recog_draw (const board_t * board);
+    };
+};
+
+#endif /*_ALINIX_KERNEL_CHESS_GAME_APP_RECOG_H*/

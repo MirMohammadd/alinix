fe60c15034cb4bb63a1c332be1cba80d54c584c8
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-11 22:19:29 -0400

    Patch

---
+++ apps/chess/move_legal.h
@@ -0,0 +1,13 @@
+#ifndef _HEISEN_KERNEL_MOVE_LEGAL_H
+#define _HEISEN_KERNEL_MOVE_LEGAL_H
+
+namespace LIBHeisenKernel{
+    namespace engine{
+        /**
+         * @brief Functions are defined here
+        */
+       
+    }
+}
+
+#endif /*_HEISEN_KERNEL_MOVE_LEGAL_H*/

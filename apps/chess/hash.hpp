cc4685c6e7606dff03adb3224b5edcfe1b2b0935
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-12 18:07:26 -0400

    Patch

--- apps/chess/hash.hpp
+++ apps/chess/hash.hpp
@@ -26,6 +26,20 @@ namespace LIBHeisenKernel
          * @brief Defining the variables 
         */
         extern uint16_t Castle[16];
+
+        /**
+         * @brief Defining the functions
+        */
+        extern void   hash_init         ();
+
+        extern uint64_t hash_key          (const board_t * board);
+        extern uint64_t hash_pawn_key     (const board_t * board);
+        extern uint64_t hash_material_key (const board_t * board);
+
+        extern uint64_t hash_piece_key    (int piece, int square);
+        extern uint64_t hash_castle_key   (int flags);
+        extern uint64_t hash_ep_key       (int square);
+        extern uint64_t hash_turn_key     (int colour);
 }}
 
 #endif /*_ALINIX_KERNEL_APPS_CHESS_GAME_H*/

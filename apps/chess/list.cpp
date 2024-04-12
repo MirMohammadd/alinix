6dd9dce9cdf665456d899250dc830645ad1b5e2a
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-12 19:14:23 -0400

    Minor patch

--- apps/chess/list.cpp
+++ apps/chess/list.cpp
@@ -4,6 +4,7 @@
 #include "util.h"
 #include "value.h"
 #include <types.h>
+#include <assert.h>
 
 using namespace LIBHeisenKernel;
 using namespace HeisenOs::common;
@@ -19,6 +20,22 @@ namespace LIBHeisenKernel{
         bool list_is_ok(const list_t * list){
             if (list == NULL) return false;
 
+            if (list->size < 0 || list->size >= ListSize) return false;
+
+            return true;
+
+        }
+
+        void list_remove(list_t* list,int pos){
+            int i;
+            assert(list_is_ok(list));
+            assert(pos>=0&& pos < list->size);
+            for (i = pos; i < list->size-1; i++) {
+            list->move[i] = list->move[i+1];
+            list->value[i] = list->value[i+1];
+        }
+
+        list->size--;
         }
     }
 };

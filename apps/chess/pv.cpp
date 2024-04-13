858bcc98048afa4e8e88d573b43b0b638e540f24
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-12 22:00:03 -0400

    Patch

--- apps/chess/pv.cpp
+++ apps/chess/pv.cpp
@@ -44,6 +44,44 @@ namespace LIBHeisenKernel{
                 ;
             }
         
-        
+        void pv_cat(mv_t dst[],const mv_t src[],int move){
+            assert(pv_is_ok(src));
+            assert(dst!=NULL);
+
+            *dst++ = move;
+            while ((*dst++ = *src++) != MoveNone)
+                ;
+        }
+
+        bool pv_to_string(const mv_t pv[], char string[], int size) {
+
+            int pos;
+            int move;
+
+            assert(pv_is_ok(pv));
+            assert(string!=NULL);
+            assert(size>=512);
+
+            // init
+
+            if (size < 512) return false;
+
+            pos = 0;
+
+            // loop
+
+            while ((move = *pv++) != MoveNone) {
+
+                if (pos != 0) string[pos++] = ' ';
+
+                move_to_string(move,&string[pos],size-pos);
+                pos += strlen(&string[pos]);
+            }
+
+            string[pos] = '\0';
+
+            return true;
+            }
+
     }
 }

3d26e178074f3d6465b77de03e0ead4e52ccabaf
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-13 15:46:24 -0400

    Minor update

--- apps/editor/editor.c
+++ apps/editor/editor.c
@@ -92,3 +92,36 @@ int getCursorPosition(int ifd, int ofd, int *rows, int *cols){
     if (sscanf(buf+2,"%d;%d",rows,cols) != 2) return -1;
     return 0;
 }
+
+int getWindowSize(int ifd, int ofd, int *rows, int *cols){
+        struct winsize ws;
+
+    if (ioctl(1, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+        /* ioctl() failed. Try to query the terminal itself. */
+        int orig_row, orig_col, retval;
+
+        /* Get the initial position so we can restore it later. */
+        retval = getCursorPosition(ifd,ofd,&orig_row,&orig_col);
+        if (retval == -1) goto failed;
+
+        /* Go to right/bottom margin and get position. */
+        if (write(ofd,"\x1b[999C\x1b[999B",12) != 12) goto failed;
+        retval = getCursorPosition(ifd,ofd,rows,cols);
+        if (retval == -1) goto failed;
+
+        /* Restore position. */
+        char seq[32];
+        snprintf(seq,32,"\x1b[%d;%dH",orig_row,orig_col);
+        if (write(ofd,seq,strlen(seq)) == -1) {
+            /* Can't recover... */
+        }
+        return 0;
+    } else {
+        *cols = ws.ws_col;
+        *rows = ws.ws_row;
+        return 0;
+    }
+
+failed:
+    return -1;
+}

6c7c5bff61632b86a2c3a6bd79797899263311c5
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-13 21:30:57 -0400

    Minor patch

--- kernel/memory/memory.c
+++ kernel/memory/memory.c
@@ -24,3 +24,36 @@ void memset(void* bufptr, char value, uint32_t size){
     }
     return bufptr;
 }
+
+int memcmp(const void* aptr, const void* bptr, uint32_t size){
+    const unsigned char* a = (const unsigned char*) aptr;
+	const unsigned char* b = (const unsigned char*) bptr;
+	for (uint32_t i = 0; i < size; i++) {
+		if (a[i] < b[i])
+			return -1;
+		else if (b[i] < a[i])
+			return 1;
+	}
+	return 0;
+}
+
+void* memmove(void* dstptr, const void* srcptr, uint32_t size){
+    unsigned char* dst = (unsigned char*) dstptr;
+	const unsigned char* src = (const unsigned char*) srcptr;
+	if (dst < src) {
+		for (uint32_t i = 0; i < size; i++)
+			dst[i] = src[i];
+	} else {
+		for (uint32_t i = size; i != 0; i--)
+			dst[i-1] = src[i-1];
+	}
+	return dstptr;
+}
+
+void* memcpy(void* dstptr, const void* srcptr, uint32_t size){
+    unsigned char* dst = (unsigned char*) dstptr;
+	const unsigned char* src = (const unsigned char*) srcptr;
+	for (uint32_t i = 0; i < size; i++)
+		dst[i] = src[i];
+	return dstptr;
+}

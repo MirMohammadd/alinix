295d466e050171a24aee1d081634bd56b7975ea2
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-14 11:49:00 -0400

    Patch

--- include/alinix/textgui.h
+++ include/alinix/textgui.h
@@ -8,9 +8,9 @@
 
 #define TEXT_COLOR VGA_COLOR_WHITE
 
-        static void DisableCursor();
-        static void SetPixel(int x, int y, char color,uint16_t character = ' ', char background = VGA_COLOR_BLUE);
-        static void ClearScreen(char color = VGA_COLOR_BLACK);
-        static void StatusBar(char* text, int percentage = 0);
-        static void DrawString(char* text, int x, int y, char color = TEXT_COLOR, char background = VGA_COLOR_BLUE);
+static void DisableCursor();
+static void SetPixel(int x, int y, char color,uint16_t character = ' ', char background = VGA_COLOR_BLUE);
+static void ClearScreen(char color = VGA_COLOR_BLACK);
+static void StatusBar(char* text, int percentage = 0);
+static void DrawString(char* text, int x, int y, char color = TEXT_COLOR, char background = VGA_COLOR_BLUE);
 #endif /*__ALINIX_KERNEL_TEXTGUI_H*/

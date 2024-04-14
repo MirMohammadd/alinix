ae736f96483a7eaa41eb7073f72b153752a7eecd
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-13 21:53:56 -0400

    Patch

--- include/alinix/syscall/kernel_implement.h
+++ include/alinix/syscall/kernel_implement.h
@@ -1,6 +1,8 @@
 #ifndef __ALINIX_KERNEL_KERNEL_IMPL_SYSCALL
 #define __ALINIX_KERNEL_KERNEL_IMPL_SYSCALL
 
+#include <alinix/register.h>
 
+static CPUState* HandleSyscall(CPUState* state);
 
 #endif /*__ALINIX_KERNEL_KERNEL_IMPL_SYSCALL*/

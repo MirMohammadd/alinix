8daad81efc79df8ff4d9cf846c2c83db9e47a1e7
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-14 11:04:37 -0400

    Patch

--- drivers/video/bootconsole.c
+++ drivers/video/bootconsole.c
@@ -1,6 +1,14 @@
 #include <alinix/bootconsole.h>
 #include <alinix/types.h>
 
+
+/// @brief ///////////////////////////////
+int XOffset = 0;
+int YOffset = 0;
+uint8_t BackgroundColor = VGA_COLOR_BLACK; //Default console background color
+uint8_t ForegroundColor = VGA_COLOR_WHITE; //Default console foreground color
+bool writeToSerial = false;
+
 static uint16_t* videoMemory = (uint16_t*)0xC00B8000;
 
 void Scroll(){
@@ -15,4 +23,8 @@ void Scroll(){
         where = (volatile uint16_t *)videoMemory + (24 * VGA_WIDTH + x) ;
         *where = ' ' | (attrib << 8);
     }
+}
+
+void Init(bool enableSerial){
+
 }

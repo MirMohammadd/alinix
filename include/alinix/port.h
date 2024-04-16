5563c1fa5443d3521d2a3e7958b250016e677377
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-16 18:18:14 -0400

    Patch for #118

--- include/alinix/port.h
+++ include/alinix/port.h
@@ -20,22 +20,22 @@
 
 #include <alinix/types.h>
 
-inline unsigned char inportb (unsigned short _port);
+unsigned char inportb (unsigned short _port);
 
-inline void outportb (unsigned short _port, unsigned char _data);
+void outportb (unsigned short _port, unsigned char _data);
 
-inline unsigned short inportw(unsigned short _port);
+unsigned short inportw(unsigned short _port);
 
-inline void outportw(unsigned short _port, unsigned short _data);
-inline unsigned int inportl(unsigned short _port);
-inline void outportl(unsigned short _port, unsigned int _data);
+void outportw(unsigned short _port, unsigned short _data);
+unsigned int inportl(unsigned short _port);
+void outportl(unsigned short _port, unsigned int _data);
 
-inline void inportsm(unsigned short port, unsigned char * data, unsigned long size);
+void inportsm(unsigned short port, unsigned char * data, unsigned long size);
 
-inline void outportsm(unsigned short port, unsigned char * data, unsigned long size) ;
+void outportsm(unsigned short port, unsigned char * data, unsigned long size) ;
 
-inline void writeMemReg(const uint32_t addr, const uint32_t val);
+void writeMemReg(const uint32_t addr, const uint32_t val);
 
-inline uint32_t readMemReg(const uint32_t addr);
+uint32_t readMemReg(const uint32_t addr);
 
 #endif // __ALINIX_KERNEL_PORT_H

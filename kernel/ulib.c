7bf5e889602ded6e93b9392e1f164a42cb14e25e
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-16 17:11:41 -0400

    Patch for #118

--- kernel/ulib.c
+++ kernel/ulib.c
@@ -1,8 +1,18 @@
 #include <alinix/types.h>
 #include <alinix/syscall.h>
-#include <stdlib.h>
 
-// void *malloc(size_t size)
-// {
-//   return (void*)syscall(SYSCALL_MEM_ALLOCATE, &size);
-// }
+
+uint syscall(uint service, void *param)
+{
+  uint __res = 0;
+  __asm__ volatile( "int $49;"
+                  : "=a" ((uint)(__res))
+                  : "0" (service), "b" ((uint)(service)),"c" ((void*)(param))
+                  : "memory"
+                );
+  return __res;
+}
+void *malloc(size_t size)
+{
+  return (void*)syscall(SYSCALL_MEM_ALLOCATE, &size);
+}

d6bd3e1f27613244d877120d2dd62204b1d774ee
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-16 16:26:39 -0400

    Minor update

--- include/alinix/string.h
+++ include/alinix/string.h
@@ -7,13 +7,13 @@ static int strlen(const char* str);
 static bool strcmp(const char* strA, const char* strB);
 static bool strncmp(const char* s1, const char* s2, int n);
 static int IndexOf(const char* str, char c, uint32_t skip);
-static bool Contains(const char* str, char c);
+// static bool Contains(const char* str, char c);
 
-            static char* Uppercase(char* str);
-            static char* Lowercase(char* str);
-            static char Uppercase(char c);
-            static char Lowercase(char c);
-            static char* strcpy(char *s1, const char *s2);
-            static char* strncpy(char *s1, const char *s2, unsigned int n);
+static char* Uppercase(char* str);
+static char* Lowercase(char* str);
+static char Uppercase(char c);
+static char Lowercase(char c);
+static char* strcpy(char *s1, const char *s2);
+static char* strncpy(char *s1, const char *s2, unsigned int n);
 
 #endif /*__ALINIX_KERNEL_STRING_H*/

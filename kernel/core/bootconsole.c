d9e284e83907ab52efccd91d446ca2808b3673d9
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-16 17:24:36 -0400

    Another patch for #118

--- kernel/core/bootconsole.c
+++ kernel/core/bootconsole.c
@@ -7,7 +7,7 @@ int XOffset = 0;
 int YOffset = 0;
 uint8_t BackgroundColor = VGA_COLOR_BLACK; //Default console background color
 uint8_t ForegroundColor = VGA_COLOR_WHITE; //Default console foreground color
-bool writeToSerial = false;
+// bool writeToSerial = false;
 
 /*/////////////////
 // Variables

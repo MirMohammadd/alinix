d3935974243a76bec715295457d104325d1a13e2
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-17 18:36:15 -0400

    Patch

---
+++ include/alinix/gui/canvas.h
@@ -0,0 +1,43 @@
+/**
+ * @author Ali Mirmohammad
+ * @file virtualfilesystem.h
+ * *************************************IMPORTANT ALINIX LICENSE TERM********************************************
+ ** This file is part of AliNix.
+
+**AliNix is free software: you can redistribute it and/or modify
+**it under the terms of the GNU Affero General Public License as published by
+**the Free Software Foundation, either version 3 of the License, or
+**(at your option) any later version.
+
+**AliNix is distributed in the hope that it will be useful,
+**but WITHOUT ANY WARRANTY; without even the implied warranty of
+**MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+**GNU Affero General Public License for more details.
+
+**You should have received a copy of the GNU Affero General Public License
+**along with AliNix. If not, see <https://www.gnu.org/licenses/>.
+*/
+#ifndef __ALINIX_KERNEL_GUI_CANVAS_H
+#define __ALINIX_KERNEL_GUI_CANVAS_H
+
+#include <alinix/types.h>
+#include <alinix/gui/fonts/font.h>
+
+
+
+#if defined(__cplusplus)
+
+class Canvas{
+    private:
+        void DrawCircleHelper(int x, int y, int radius, uint32_t corner, uint32_t color);
+        void FillCircleHelper(int x, int y, int radius, uint32_t corner, int delta, uint32_t color);
+};
+
+#else 
+
+void DrawCircleHelper(int x, int y, int radius, uint32_t corner, uint32_t color);
+void FillCircleHelper(int x, int y, int radius, uint32_t corner, int delta, uint32_t color);
+
+#endif /*__cplusplus*/
+
+#endif /*__ALINIX_KERNEL_GUI_CANVAS_H*/

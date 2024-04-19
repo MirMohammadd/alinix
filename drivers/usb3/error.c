e5e848b77bab2eaaa836aecd20e76722d583774f
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-19 19:57:57 -0400

    Patch

--- drivers/usb3/error.c
+++ drivers/usb3/error.c
@@ -1,73 +1,73 @@
-/**
- ** This file is part of AliNix.
+// /**
+//  ** This file is part of AliNix.
 
-**AliNix is free software: you can redistribute it and/or modify
-**it under the terms of the GNU Affero General Public License as published by
-**the Free Software Foundation, either version 3 of the License, or
-**(at your option) any later version.
+// **AliNix is free software: you can redistribute it and/or modify
+// **it under the terms of the GNU Affero General Public License as published by
+// **the Free Software Foundation, either version 3 of the License, or
+// **(at your option) any later version.
 
-**AliNix is distributed in the hope that it will be useful,
-**but WITHOUT ANY WARRANTY; without even the implied warranty of
-**MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-**GNU Affero General Public License for more details.
+// **AliNix is distributed in the hope that it will be useful,
+// **but WITHOUT ANY WARRANTY; without even the implied warranty of
+// **MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// **GNU Affero General Public License for more details.
 
-**You should have received a copy of the GNU Affero General Public License
-**along with AliNix. If not, see <https://www.gnu.org/licenses/>.
-*/
-
-
-#include <libusbserial.h>
-static const char* ERROR_MSG_UNSUPPORTED_OPERATION = "Unsupported operation";
-static const char* ERROR_MSG_ILLEGAL_STATE = "Illegal state";
-static const char* ERROR_MSG_INVALID_PARAMETER = "Invalid parameter";
-static const char* ERROR_MSG_RESOURCE_ALLOC_FAILED = "Resource allocation failed";
-static const char* ERROR_MSG_NO_SUCH_DEVICE = "No such device";
-static const char* ERROR_MSG_UNSUPPORTED_DEVICE = "Unsupported device";
-static const char* ERROR_MSG_UNSUPPORTED_BAUD_RATE = "Unsupported baud rate";
-static const char* ERROR_MSG_INVALID_PORT_IDX = "Invalid port index";
-static const char* ERROR_MSG_CTRL_CMD_FAILED = "Control command failed";
-
-static const char* ERROR_MSG_UNKNOWN = "Unknown error";
+// **You should have received a copy of the GNU Affero General Public License
+// **along with AliNix. If not, see <https://www.gnu.org/licenses/>.
+// */
 
 
-const char* usbserial_get_error_str(int error_code){
-    if (!error_code){
-        return NULL;
-    }
+// #include <libusbserial.h>
+// static const char* ERROR_MSG_UNSUPPORTED_OPERATION = "Unsupported operation";
+// static const char* ERROR_MSG_ILLEGAL_STATE = "Illegal state";
+// static const char* ERROR_MSG_INVALID_PARAMETER = "Invalid parameter";
+// static const char* ERROR_MSG_RESOURCE_ALLOC_FAILED = "Resource allocation failed";
+// static const char* ERROR_MSG_NO_SUCH_DEVICE = "No such device";
+// static const char* ERROR_MSG_UNSUPPORTED_DEVICE = "Unsupported device";
+// static const char* ERROR_MSG_UNSUPPORTED_BAUD_RATE = "Unsupported baud rate";
+// static const char* ERROR_MSG_INVALID_PORT_IDX = "Invalid port index";
+// static const char* ERROR_MSG_CTRL_CMD_FAILED = "Control command failed";
 
-    if ((-1 >= error_code) && (-99 <= error_code)){
-        return libusb_error_name(error_code);
-    }
+// static const char* ERROR_MSG_UNKNOWN = "Unknown error";
 
-    switch (error_code){
-        case USBSERIAL_ERROR_UNSUPPORTED_OPERATION  :
-            return ERROR_MSG_UNSUPPORTED_OPERATION;
+
+// const char* usbserial_get_error_str(int error_code){
+//     if (!error_code){
+//         return NULL;
+//     }
+
+//     if ((-1 >= error_code) && (-99 <= error_code)){
+//         return libusb_error_name(error_code);
+//     }
+
+//     switch (error_code){
+//         case USBSERIAL_ERROR_UNSUPPORTED_OPERATION  :
+//             return ERROR_MSG_UNSUPPORTED_OPERATION;
         
-        case USBSERIAL_ERROR_ILLEGAL_STATE:
-            return ERROR_MSG_ILLEGAL_STATE;
+//         case USBSERIAL_ERROR_ILLEGAL_STATE:
+//             return ERROR_MSG_ILLEGAL_STATE;
         
-        case USBSERIAL_ERROR_INVALID_PARAMETER:
-            return ERROR_MSG_INVALID_PARAMETER;
+//         case USBSERIAL_ERROR_INVALID_PARAMETER:
+//             return ERROR_MSG_INVALID_PARAMETER;
         
-        case USBSERIAL_ERROR_RESOURCE_ALLOC_FAILED:
-            return  ERROR_MSG_RESOURCE_ALLOC_FAILED;
+//         case USBSERIAL_ERROR_RESOURCE_ALLOC_FAILED:
+//             return  ERROR_MSG_RESOURCE_ALLOC_FAILED;
         
-        case USBSERIAL_ERROR_NO_SUCH_DEVICE:
-            return ERROR_MSG_NO_SUCH_DEVICE;
+//         case USBSERIAL_ERROR_NO_SUCH_DEVICE:
+//             return ERROR_MSG_NO_SUCH_DEVICE;
         
-        case USBSERIAL_ERROR_UNSUPPORTED_DEVICE:
-            return ERROR_MSG_UNSUPPORTED_DEVICE;
+//         case USBSERIAL_ERROR_UNSUPPORTED_DEVICE:
+//             return ERROR_MSG_UNSUPPORTED_DEVICE;
 
-        case USBSERIAL_ERROR_UNSUPPORTED_BAUD_RATE:
-		return ERROR_MSG_UNSUPPORTED_BAUD_RATE;
+//         case USBSERIAL_ERROR_UNSUPPORTED_BAUD_RATE:
+// 		return ERROR_MSG_UNSUPPORTED_BAUD_RATE;
 
-        case USBSERIAL_ERROR_INVALID_PORT_IDX:
-            return ERROR_MSG_INVALID_PORT_IDX;
+//         case USBSERIAL_ERROR_INVALID_PORT_IDX:
+//             return ERROR_MSG_INVALID_PORT_IDX;
 
-        case USBSERIAL_ERROR_CTRL_CMD_FAILED:
-            return ERROR_MSG_CTRL_CMD_FAILED;
+//         case USBSERIAL_ERROR_CTRL_CMD_FAILED:
+//             return ERROR_MSG_CTRL_CMD_FAILED;
         
-        default:
-            return ERROR_MSG_UNKNOWN;
-    }
-}
+//         default:
+//             return ERROR_MSG_UNKNOWN;
+//     }
+// }

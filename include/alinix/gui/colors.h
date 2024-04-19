005dcc200210f9ca70a995f91c0912a9a6e44deb
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-18 21:21:24 -0400

    Minor update

---
+++ include/alinix/gui/colors.h
@@ -0,0 +1,8 @@
+#ifndef __ALINIX_KERNEL_GUI_COLORS_H
+#define __ALINIX_KERNEL_GUI_COLORS_H
+
+#include <alinix/types.h>
+
+static const uint32_t Transparent = 0x00000000;
+
+#endif /*__ALINIX_KERNEL_GUI_COLORS_H*/

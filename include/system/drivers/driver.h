93570c7a7431d86805d72919909b44e1545b1921
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-19 16:46:35 -0400

    Patch

---
+++ include/system/drivers/driver.h
@@ -0,0 +1,11 @@
+#ifndef __ALINIX_KERNEL_SYSTEM_DRIVER_H
+#define __ALINIX_KERNEL_SYSTEM_DRIVER_H
+
+#include <alinix/types.h>
+
+char* GetDriverName();
+char* GetDriverDescription();
+
+bool Initialize();
+
+#endif /*__ALINIX_KERNEL_SYSTEM_DRIVER_H*/

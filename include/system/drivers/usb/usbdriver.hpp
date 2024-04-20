920e1d0d307fff95ddbaf2e1e8ca5611d4a4f7f5
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-20 12:13:51 -0400

    Patch

--- include/system/drivers/usb/usbdriver.hpp
+++ include/system/drivers/usb/usbdriver.hpp
@@ -23,7 +23,7 @@ public:
     // Other member functions
     virtual void DeInitialize();
     virtual bool HandleInterruptPacket(InterruptTransfer* transfer);
-    static USBDevice* device; // Pointer to USBDevice
+    // static USBDevice* device; // Pointer to USBDevice
 };
 
 

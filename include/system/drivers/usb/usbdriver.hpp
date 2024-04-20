2cb5bb405475bb660d5f00b75fdd00dcebbab429
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-19 21:06:36 -0400

    Patch

--- include/system/drivers/usb/usbdriver.hpp
+++ include/system/drivers/usb/usbdriver.hpp
@@ -9,24 +9,22 @@
 
         typedef struct InterruptTransfer InterruptTransfer_t;
 
-        class USBDriver : public Driver
-        {
-            friend class USBDevice;
-        public:
+class USBDriver : public Driver
+{
+public:
+    // Constructor
+    USBDriver(USBDevice* dev, char* driverName);
 
-            // Which device is this driver for
-            USBDevice* device;
-        public:
-            USBDriver(USBDevice* dev, char* driverName);
-            virtual ~USBDriver();
+    // Destructor
+    virtual ~USBDriver();
 
-            // De-Active this driver from the system
-            // Called when device is unplugged
-            virtual void DeInitialize();
+    // Other member functions
+    virtual void DeInitialize();
+    virtual bool HandleInterruptPacket(InterruptTransfer_t* transfer);
 
-            // Called from USB Controller when a interrupt packet is received
-            virtual bool HandleInterruptPacket(InterruptTransfer_t* transfer);
-        };
+private:
+    USBDevice* device; // Pointer to USBDevice
+};
 
 
 #endif /*__ALINIX_KERNEL_USB_DRIVER_SYSTEM_HPP*/

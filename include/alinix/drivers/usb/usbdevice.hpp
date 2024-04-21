85fdb8bd4bf36a91dfd7990932eea3ae6686e5ad
Heisenberg <97875042+MirMohammadd@users.noreply.github.com>
2024-04-21 00:12:26 -0400

    Revert "Patch"

---
+++ include/alinix/drivers/usb/usbdevice.hpp
@@ -0,0 +1,12 @@
+#ifndef __ALINIX_KERNEL_DRIVERS_USB_USB_DEVICE_HPP
+#define __ALINIX_KERNEL_DRIVERS_USB_USB_DEVICE_HPP
+
+namespace alinix{
+    namespace drivers{
+        namespace usb{
+            
+        }
+    }
+}
+
+#endif /*__ALINIX_KERNEL_DRIVERS_USB_USB_DEVICE_HPP*/

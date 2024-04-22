8f23d23e883cd200d08aa2bfe430d4a09eaff55c
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-22 18:20:48 -0400

    Update

--- include/net/bpf_tcp_helpers.h
+++ include/net/bpf_tcp_helpers.h
@@ -18,4 +18,18 @@ struct sock_common {
 	uint16_t		skc_num;
 } __attribute__((preserve_access_index));
 
+enum sk_pacing {
+	SK_PACING_NONE		= 0,
+	SK_PACING_NEEDED	= 1,
+	SK_PACING_FQ		= 2,
+};
+
+
+struct sock {
+	struct sock_common	__sk_common;
+#define sk_state		__sk_common.skc_state
+	unsigned long		sk_pacing_rate;
+	uint32_t			sk_pacing_status; /* see enum sk_pacing */
+} __attribute__((preserve_access_index));
+
 #endif // __ALINIX_KERNEL_BPF_TCP_HELPERS_H

bf8fdcaa29ef1c5a5e8a3f191bcfebcd5d952572
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-23 19:40:41 -0400

    Update

---
+++ include/net/err.h
@@ -0,0 +1,10 @@
+#ifndef __ALINIX_KERNEL_NET_INCLUDED_ERR_H
+#define __ALINIX_KERNEL_NET_INCLUDED_ERR_H
+
+#include <alinix/types.h>
+
+
+typedef sint8_t err_t;
+
+
+#endif // 
--- include/net/udp.h
+++ include/net/udp.h
@@ -5,6 +5,7 @@
 #include <alinix/compiler.h>
 #include <net/ip_addr.h>
 #include <net/ip.h>
+#include <net/err.h>
 
 
 #ifdef __cplusplus
@@ -60,6 +61,56 @@ struct udp_pcb {
   /** user-supplied argument for the recv callback */
   void *recv_arg;  
 };
+
+extern struct udp_pcb *udp_pcbs;
+
+
+void             udp_remove     (struct udp_pcb *pcb);
+err_t            udp_bind       (struct udp_pcb *pcb, ip_addr_t *ipaddr,
+                                 uint16_t port);
+err_t            udp_connect    (struct udp_pcb *pcb, ip_addr_t *ipaddr,
+                                 uint16_t port);
+void             udp_disconnect (struct udp_pcb *pcb);
+void             udp_recv       (struct udp_pcb *pcb, udp_recv_fn recv,
+                                 void *recv_arg);
+err_t            udp_sendto_if  (struct udp_pcb *pcb, struct pbuf *p,
+                                 ip_addr_t *dst_ip, uint16_t dst_port,
+                                 struct netif *netif);
+err_t            udp_sendto     (struct udp_pcb *pcb, struct pbuf *p,
+                                 ip_addr_t *dst_ip, uint16_t dst_port);
+err_t            udp_send       (struct udp_pcb *pcb, struct pbuf *p);
+
+
+
+
+#if LWIP_CHECKSUM_ON_COPY
+err_t            udp_sendto_if_chksum(struct udp_pcb *pcb, struct pbuf *p,
+                                 ip_addr_t *dst_ip, u16_t dst_port,
+                                 struct netif *netif, uint8_t have_chksum,
+                                 u16_t chksum);
+err_t            udp_sendto_chksum(struct udp_pcb *pcb, struct pbuf *p,
+                                 ip_addr_t *dst_ip, u16_t dst_port,
+                                 uint8_t have_chksum, u16_t chksum);
+err_t            udp_send_chksum(struct udp_pcb *pcb, struct pbuf *p,
+                                 uint8_t have_chksum, u16_t chksum);
+#endif /* LWIP_CHECKSUM_ON_COPY */
+
+#define          udp_flags(pcb) ((pcb)->flags)
+#define          udp_setflags(pcb, f)  ((pcb)->flags = (f))
+
+/* The following functions are the lower layer interface to UDP. */
+void             udp_input      (struct pbuf *p, struct netif *inp);
+
+void             udp_init       (void);
+
+#if UDP_DEBUG
+void udp_debug_print(struct udp_hdr *udphdr);
+#else
+#define udp_debug_print(udphdr)
+#endif
+
+
+
 
 #ifdef __cplusplus
 }

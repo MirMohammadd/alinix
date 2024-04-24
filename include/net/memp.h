b36063dcf26ce8d778ce438ba3c3ef74a4737a5f
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-24 13:26:34 -0400

    New update

--- include/net/memp.h
+++ include/net/memp.h
@@ -3,10 +3,13 @@
 
 
 #include <alinix/types.h>
+#include <alinix/memory.h>
+
+extern const uint16_t memp_sizes[MEMP_MAX];
+
 
 #define memp_malloc(type)     mem_malloc(memp_sizes[type])
 
 
-extern const uint16_t memp_sizes[MEMP_MAX];
 
 #endif

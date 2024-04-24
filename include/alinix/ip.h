4a54fd9cc583c59967730454f692e3fff2165724
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-24 17:19:44 -0400

    Patch

--- include/alinix/ip.h
+++ include/alinix/ip.h
@@ -33,5 +33,22 @@
 
 #define IP_PCB_ADDRHINT ;uint8_t addr_hint
 
+#define IP_PCB \
+  /* ip addresses in network byte order */ \
+  ip_addr_t local_ip; \
+  ip_addr_t remote_ip; \
+   /* Socket options */  \
+  uint8_t so_options;      \
+   /* Type Of Service */ \
+  uint8_t tos;              \
+  /* Time To Live */     \
+  uint8_t ttl               \
+  /* link layer address resolution hint */ \
+  IP_PCB_ADDRHINT
+
+struct ip_pcb {
+/* Common members of all PCB types */
+  IP_PCB;
+};
 
 #endif /*__ALINIX_KERNEL_IP_H*/

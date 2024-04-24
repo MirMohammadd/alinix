bb4c1132cd62e9cd5d934180074f82b64f2e963d
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-23 22:23:28 -0400

    Patch

--- include/net/def.h
+++ include/net/def.h
@@ -4,7 +4,15 @@
 
 #include <alinix/types.h>
 
+uint16_t lwip_htons(uint16_t x);
+uint16_t lwip_ntohs(uint16_t x);
+uint32_t lwip_htonl(uint32_t x);
+uint32_t lwip_ntohl(uint32_t x);
+
+
 #define ntohl(x) lwip_ntohl(x)
+
+
 
 uint32_t __always_inline
 lwip_ntohl(uint32_t n)
@@ -16,18 +24,8 @@ lwip_ntohl(uint32_t n)
 
 
 
-#if LWIP_PLATFORM_BYTESWAP
-#define lwip_htons(x) LWIP_PLATFORM_HTONS(x)
-#define lwip_ntohs(x) LWIP_PLATFORM_HTONS(x)
-#define lwip_htonl(x) LWIP_PLATFORM_HTONL(x)
-#define lwip_ntohl(x) LWIP_PLATFORM_HTONL(x)
-#else /* LWIP_PLATFORM_BYTESWAP */
-uint16_t lwip_htons(uint16_t x);
-uint16_t lwip_ntohs(uint16_t x);
-uint32_t lwip_htonl(uint32_t x);
-uint32_t lwip_ntohl(uint32_t x);
 
-#endif /* LWIP_PLATFORM_BYTESWAP */
+
 
 
 #define LWIP_MAX(x , y)  (((x) > (y)) ? (x) : (y))

013a1446260cc7098317030e22a6930d671c658e
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-24 18:46:42 -0400

    Patch

--- include/net/netif.h
+++ include/net/netif.h
@@ -27,6 +27,7 @@
 extern "C" {
 #endif
 
+
 /** Function prototype for netif status- or link-callback functions. */
 typedef void (*netif_status_callback_fn)(struct netif *netif);
 /** Function prototype for netif igmp_mac_filter functions */

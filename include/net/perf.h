8754884f89cfa15dfe0b829a958705ac76daf506
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-24 13:11:51 -0400

    Update patch

--- include/net/perf.h
+++ include/net/perf.h
@@ -2,15 +2,15 @@
 #define __ALINIX_KERNEL_NET_PERF_H
 
 
-inline void perf_print(unsigned long c1l, unsigned long c1h,
-		unsigned long c2l, unsigned long c2h,
-		char *key);
+// void perf_print(unsigned long c1l, unsigned long c1h,
+// 		unsigned long c2l, unsigned long c2h,
+// 		char *key);
 
 
 #define PERF_START  { \
                          unsigned long __c1l, __c1h, __c2l, __c2h; \
                          __asm__(".byte 0x0f, 0x31" : "=a" (__c1l), "=d" (__c1h))
 #define PERF_STOP(x)   __asm__(".byte 0x0f, 0x31" : "=a" (__c2l), "=d" (__c2h)); \
-                       perf_print(__c1l, __c1h, __c2l, __c2h, x);}
+                       }
 
 #endif

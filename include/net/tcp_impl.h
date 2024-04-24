a5051a1675f4b4d269fca003b3c1ba610e4f0ddb
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-24 17:12:51 -0400

    Patch

--- include/net/tcp_impl.h
+++ include/net/tcp_impl.h
@@ -4,5 +4,8 @@
 #include <alinix/types.h>
 
 extern uint8_t tcp_active_pcbs_changed;
+extern struct tcp_pcb *tcp_active_pcbs;  /* List of all TCP PCBs that are in a
+              state in which they accept or send
+              data. */
 
 #endif

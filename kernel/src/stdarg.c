e01b7334015ee935c397ad9eb5c27be13c03bf51
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-24 12:54:19 -0400

    Patch

--- kernel/src/stdarg.c
+++ kernel/src/stdarg.c
@@ -2,16 +2,16 @@
 
 int printf (const char *format, ...)
 {
-    va_list ap;
-    int len=0;
+    // va_list ap;
+    // int len=0;
     	
-    /* Start variabile argument's list */
-    va_start (ap, format);
-	char buffer[1024];
+    // /* Start variabile argument's list */
+    // va_start (ap, format);
+	// char buffer[1024];
 
-	len = vsprintf(buffer, format, ap);
-	_kputs(buffer);
-    va_end (ap); // end of arguments
+	// len = vsprintf(buffer, format, ap);
+	// _kputs(buffer);
+    // va_end (ap); // end of arguments
 
-    return len;
+    // return len;
 }

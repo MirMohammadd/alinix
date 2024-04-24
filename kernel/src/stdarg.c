b8c7362f8c315ce9ab9e5b6f3929ce9129bfec6f
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-23 21:34:52 -0400

    Patch

---
+++ kernel/src/stdarg.c
@@ -0,0 +1,17 @@
+#include <alinix/stdarg.h>
+
+int printf (const char *format, ...)
+{
+    va_list ap;
+    int len=0;
+    	
+    /* Start variabile argument's list */
+    va_start (ap, format);
+	char buffer[1024];
+
+	len = vsprintf(buffer, format, ap);
+	_kputs(buffer);
+    va_end (ap); // end of arguments
+
+    return len;
+}

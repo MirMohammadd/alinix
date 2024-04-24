697e6ed31c115e81a63418814de6f701a1329be1
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-24 18:25:05 -0400

    Patch

--- include/net/ip.h
+++ include/net/ip.h
@@ -21,25 +21,25 @@
 #include <net/ip_addr.h>
 
 
-#define IP_PCB \
-  struct ip_pcb;
-  typedef struct ip_addr_t;
-  /* ip addresses in network byte order */ \
-  ip_addr_t local_ip; \
-  ip_addr_t remote_ip; \
-   /* Socket options */  \
-  uint8_t so_options;      \
-   /* Type Of Service */ \
-  uint8_t tos;              \
-  /* Time To Live */     \
-  uint8_t ttl               \
-  /* link layer address resolution hint */ \
-//   IP_PCB_ADDRHINT
+// #define IP_PCB \
+//   struct ip_pcb;
+//   typedef struct ip_addr_t;
+//   /* ip addresses in network byte order */ \
+//   ip_addr_t local_ip; \
+//   ip_addr_t remote_ip; \
+//    /* Socket options */  \
+//   uint8_t so_options;      \
+//    /* Type Of Service */ \
+//   uint8_t tos;              \
+//   /* Time To Live */     \
+//   uint8_t ttl               \
+//   /* link layer address resolution hint */ \
+// //   IP_PCB_ADDRHINT
 
-struct ip_pcb {
-/* Common members of all PCB types */
-  IP_PCB ;
-};
+// struct ip_pcb {
+// /* Common members of all PCB types */
+//   IP_PCB ;
+// };
 
 
 #endif // __ALINIX_KERNEL_NET_IP_H_INCLUDED

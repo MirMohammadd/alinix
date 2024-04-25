ea4da7590da1c1252781ed215ab33aeb9158199f
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-24 20:10:04 -0400

    Update

--- include/net/dhcp.h
+++ include/net/dhcp.h
@@ -115,32 +115,7 @@ struct dhcp_msg
 PACK_STRUCT_END
 
 
-void dhcp_set_struct(struct netif *netif, struct dhcp *dhcp);
-#define dhcp_remove_struct(netif) do {(netif)->dhcp = NULL;} while (0)
-void dhcp_cleanup(struct netif *netif);
-/** start DHCP configuration */
-err_t dhcp_start(struct netif *netif);
-/** enforce early lease renewal (not needed normally)*/
-err_t dhcp_renew(struct netif *netif);
-/** release the DHCP lease, usually called before dhcp_stop()*/
-err_t dhcp_release(struct netif *netif);
-/** stop DHCP configuration */
-void dhcp_stop(struct netif *netif);
-/** inform server of our manual IP address */
-void dhcp_inform(struct netif *netif);
-/** Handle a possible change in the network configuration */
-void dhcp_network_changed(struct netif *netif);
 
-
-/** if enabled, check whether the offered IP address is not in use, using ARP */
-#if DHCP_DOES_ARP_CHECK
-void dhcp_arp_reply(struct netif *netif, ip_addr_t *addr);
-#endif
-
-/** to be called every minute */
-void dhcp_coarse_tmr(void);
-/** to be called every half second */
-void dhcp_fine_tmr(void);
  
 /** DHCP message item offsets and length */
 #define DHCP_OP_OFS       0

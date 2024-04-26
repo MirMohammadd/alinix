f233c52ab46acde8dafe24ff50e70b96f49ca2c3
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-26 19:16:40 -0400

    Patch

--- kernel/drivers/firmware/vsprintf.c
+++ kernel/drivers/firmware/vsprintf.c
@@ -22,7 +22,6 @@
 #include <alinix/stdarg.h>
 #include <alinix/compiler.h>
 #include <alinix/limits.h>
-#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
 
 
 
@@ -104,7 +103,7 @@ int get_flags(const char **fmt){
 int vsnprintf(char *buf, size_t size, const char *fmt, va_list ap){
     /*Maximum pace needed to print 64 bit number in octal*/
     char tmp[(sizeof(unsigned long long) * 8+2) / 3];
-    char *tmp_end = &tmp[ARRAY_SIZE[tmp]];
+    char *tmp_end = &tmp[sizeof(tmp) / sizeof(tmp)[0]];
     long long num;
     int base;
     const char *s;

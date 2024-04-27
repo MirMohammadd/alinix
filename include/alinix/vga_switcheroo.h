bf6b93090c75af96216db6694e5df8f8afb8524c
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-27 17:26:10 -0400

    Update

---
+++ include/alinix/vga_switcheroo.h
@@ -0,0 +1,20 @@
+#ifndef __ALINIX_KERNEL_VGA_SWITCH_HEROO_H
+#define __ALINIX_KERNEL_VGA_SWITCH_HEROO_H
+
+enum vga_switcheroo_client_id {
+	VGA_SWITCHEROO_UNKNOWN_ID = 0x1000,
+	VGA_SWITCHEROO_IGD = 0,
+	VGA_SWITCHEROO_DIS,
+	VGA_SWITCHEROO_MAX_CLIENTS,
+};
+
+struct vga_switcheroo_handler {
+	int (*init)(void);
+	int (*switchto)(enum vga_switcheroo_client_id id);
+	int (*switch_ddc)(enum vga_switcheroo_client_id id);
+	int (*power_state)(enum vga_switcheroo_client_id id,
+			   enum vga_switcheroo_state state);
+	enum vga_switcheroo_client_id (*get_client_id)(struct pci_dev *pdev);
+};
+
+#endif
--- kernel/drivers/gpu/vga/vga_switcheroo.c
+++ kernel/drivers/gpu/vga/vga_switcheroo.c
@@ -2,13 +2,14 @@
 #include <alinix/kernel.h>
 #include <alinix/compiler.h>
 #include <alinix/fs/fat.h>
+#include <alinix/vga_switcheroo.h>
 
 
 struct vgasr_priv{
     bool active;
     bool delayed_switch_active;
     int registered_clients;
-    
+
 };
 
 PRIVATE bool vga_switcheroo_ready(NO_ARGS){

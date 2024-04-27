385cc8784acd39b9097ece002819c94f51bb3da1
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-27 19:59:32 -0400

    Update

--- kernel/lib/kobject.c
+++ kernel/lib/kobject.c
@@ -2,27 +2,3 @@
 #include <alinix/kobject.h>
 #include <alinix/kern_levels.h>
 
-void kobject_put(struct kobject *kobj)
-{
-	if (kobj) {
-		if (!kobj->state_initialized)
-			WARN(1, KERN_WARNING
-				"kobject: '%s' (%p): is not initialized, yet kobject_put() is being called.\n",
-			     kobject_name(kobj), kobj);
-		kref_put(&kobj->kref, kobject_release);
-	}
-}
-
-
-void kobject_del(struct kobject *kobj)
-{
-	struct kobject *parent;
-
-	if (!kobj)
-		return;
-
-	parent = kobj->parent;
-	__kobject_del(kobj);
-	kobject_put(parent);
-}
-

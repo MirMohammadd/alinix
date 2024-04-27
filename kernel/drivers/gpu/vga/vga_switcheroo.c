1fc24b3ec800222d2365bd2ba886aacf90c2afb5
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-27 17:28:46 -0400

    Update

--- kernel/drivers/gpu/vga/vga_switcheroo.c
+++ kernel/drivers/gpu/vga/vga_switcheroo.c
@@ -9,9 +9,10 @@ struct vgasr_priv{
     bool active;
     bool delayed_switch_active;
     int registered_clients;
-
+    const struct vga_switcheroo_handler *handler;
 };
 
 PRIVATE bool vga_switcheroo_ready(NO_ARGS){
-
+    struct vgasr_priv *priv;
+    return !priv->active && priv->registered_clients == 2 && priv->handler;
 }

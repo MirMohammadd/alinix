40b3484709b9f789aecf1a34c58e2bc714cf6946
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-28 18:29:03 -0400

    Update

---
+++ kernel/lib/dim/dim.c
@@ -0,0 +1,32 @@
+/**
+ ** This file is part of AliNix.
+
+**AliNix is free software: you can redistribute it and/or modify
+**it under the terms of the GNU Affero General Public License as published by
+**the Free Software Foundation, either version 3 of the License, or
+**(at your option) any later version.
+
+**AliNix is distributed in the hope that it will be useful,
+**but WITHOUT ANY WARRANTY; without even the implied warranty of
+**MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+**GNU Affero General Public License for more details.
+
+**You should have received a copy of the GNU Affero General Public License
+**along with AliNix. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include <alinix/dim.h>
+#include <alinix/kernel.h>
+
+
+bool dim_on_top(struct dim *dim){
+    switch(dim->tune_state){
+	case DIM_PARKING_ON_TOP:
+	case DIM_PARKING_TIRED:
+		return true;
+	case DIM_GOING_RIGHT:
+		return (dim->steps_left > 1) && (dim->steps_right == 1);
+	default: /* DIM_GOING_LEFT */
+		return (dim->steps_right > 1) && (dim->steps_left == 1);
+    }
+}

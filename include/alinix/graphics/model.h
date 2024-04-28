ac68d13940a3e8295db8228f52b62ea0826193a1
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-27 21:18:28 -0400

    Minor update

--- include/alinix/graphics/model.h
+++ include/alinix/graphics/model.h
@@ -1190,9 +1190,9 @@
     #define REG_JPEG_DRI_CONFIG0            0x9f8
     #define REG_JPEG_DRI_CONFIG1            0x9fa
 
-#define DISPLAY_CS(state)					(state ? mPORTGSetBits(BIT_13) : mPORTGClearBits(BIT_13))		//0 = Select Controller
-#define DISPLAY_REGISTER_SELECT(state)		(state ? mPORTBSetBits(BIT_10) : mPORTBClearBits(BIT_10))		//0 = Command, 1 = Data
-#define	DISPLAY_RESET(state)				(state ? mPORTASetBits(BIT_10) : mPORTAClearBits(BIT_10))		//0 = Reset Controller
+#define DISPLAY_CS(state)					(state ? BIT_13 : BIT_13)		//0 = Select Controller
+#define DISPLAY_REGISTER_SELECT(state)		(state ? BIT_10 : BIT_10)		//0 = Command, 1 = Data
+#define	DISPLAY_RESET(state)				(state ? BIT_10 : BIT_10)		//0 = Reset Controller
 
 #define	DISPLAY_WRITE_DATA(data)									//Write 16bit word to controller
 

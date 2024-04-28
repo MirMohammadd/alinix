f742b70ed7df9e8f3fa60ded89e6d941476eefaa
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-27 23:44:49 -0400

    Patch

--- kernel/security/bpf/hooks.c
+++ kernel/security/bpf/hooks.c
@@ -1,3 +1,16 @@
 #include <alinix/types.h>
 #include <alinix/kernel.h>
 #include <alinix/compiler.h>
+#include <alinix/lsm_hooks.h>
+
+/**
+ * @ref https://github.com/torvalds/linux/blob/master/security/bpf/hooks.c
+*/
+
+static struct security_hook_list bpf_lsm_hooks[] = {
+    #define LSM_HOOK(RET, DEFAULT, NAME, ...)  \
+    LSM_HOOK_INIT(NAME, bpf_lsm_##NAME),
+    // LSM_HOOK_INIT(inode_free_security, bpf_inode_storage_free),
+	// LSM_HOOK_INIT(task_free, bpf_task_storage_free),
+}
+

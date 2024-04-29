36401a535413f3906a76dc0037b04855754b8498
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-29 17:56:58 -0400

    Update

---
+++ include/alinix/list_lru.h
@@ -0,0 +1,34 @@
+#ifndef __ALINIX_KERNEL_LIST_LRU_H
+#define __ALINIX_KERNEL_LIST_LRU_H
+
+#include <alinix/types.h>
+#include <alinix/list.h>
+#include <alinix/list_bl.h>
+#include <alinix/spinlock_types.h>
+
+struct list_lru_one {
+	struct list_head	list;
+	/* may become negative during memcg reparenting */
+	long			nr_items;
+};
+
+struct list_lru_node {
+	/* protects all lists on the node, including per cgroup */
+	spinlock_t		lock;
+	/* global list, used for the root cgroup in cgroup aware lrus */
+	struct list_lru_one	lru;
+	long			nr_items;
+} ____cacheline_aligned_in_smp;
+
+
+struct list_lru {
+	struct list_lru_node	*node;
+#ifdef CONFIG_MEMCG_KMEM
+	struct list_head	list;
+	int			shrinker_id;
+	bool			memcg_aware;
+	struct xarray		xa;
+#endif
+};
+
+#endif

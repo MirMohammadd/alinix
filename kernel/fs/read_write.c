13ca85a7d7ecf465019c3c43ebdf9d84037adb48
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-29 18:16:41 -0400

    Patch

--- kernel/fs/read_write.c
+++ kernel/fs/read_write.c
@@ -1,16 +1,17 @@
 #include <alinix/fs.h>
+#include <alinix/uio.h>
 
-ssize_t filemap_splice_read(struct file *in, loff_t *ppos,
+extern  filemap_splice_read(struct file *in, loff_t *ppos,
 			    struct pipe_inode_info *pipe,
 			    size_t len, unsigned int flags);
-int generic_file_readonly_mmap(struct file *, struct vm_area_struct *);
-loff_t generic_file_llseek(struct file *file, loff_t offset, int whence);
-ssize_t generic_file_read_iter(struct kiocb *, struct iov_iter *);
-ssize_t __generic_file_write_iter(struct kiocb *, struct iov_iter *);
-ssize_t generic_file_write_iter(struct kiocb *, struct iov_iter *);
-ssize_t generic_file_direct_write(struct kiocb *, struct iov_iter *);
-ssize_t generic_perform_write(struct kiocb *, struct iov_iter *);
-ssize_t direct_write_fallback(struct kiocb *iocb, struct iov_iter *iter,
+extern int generic_file_readonly_mmap(struct file *, struct vm_area_struct *);
+extern loff_t generic_file_llseek(struct file *file, loff_t offset, int whence);
+extern ssize_t generic_file_read_iter(struct kiocb *, struct iov_iter *);
+extern ssize_t __generic_file_write_iter(struct kiocb *, struct iov_iter *);
+extern ssize_t generic_file_write_iter(struct kiocb *, struct iov_iter *);
+extern ssize_t generic_file_direct_write(struct kiocb *, struct iov_iter *);
+extern ssize_t generic_perform_write(struct kiocb *, struct iov_iter *);
+extern ssize_t direct_write_fallback(struct kiocb *iocb, struct iov_iter *iter,
 		ssize_t direct_written, ssize_t buffered_written);
 
 const struct file_operations generic_ro_fops = {

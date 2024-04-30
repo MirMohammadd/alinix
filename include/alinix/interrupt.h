bfd9062f2c8096c447a4b9b8d6215a81045824cd
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-30 18:34:48 -0400

    Updates

--- include/alinix/interrupt.h
+++ include/alinix/interrupt.h
@@ -1,11 +1,36 @@
 #ifndef __ALINIX_KERNEL_INTERRUPT_H
 #define __ALINIX_KERNEL_INTERRUPT_H
 
+
+
+
+
 enum irqchip_irq_state {
 	IRQCHIP_STATE_PENDING,		/* Is interrupt pending? */
 	IRQCHIP_STATE_ACTIVE,		/* Is interrupt in progress? */
 	IRQCHIP_STATE_MASKED,		/* Is interrupt masked? */
 	IRQCHIP_STATE_LINE_LEVEL,	/* Is IRQ line high? */
 };
+
+struct irqaction {
+	irq_handler_t		handler;
+	void			*dev_id;
+	void 	*percpu_dev_id;
+	struct irqaction	*next;
+	irq_handler_t		thread_fn;
+	// struct task_struct	*thread;
+	struct irqaction	*secondary;
+	unsigned int		irq;
+	unsigned int		flags;
+	unsigned long		thread_flags;
+	unsigned long		thread_mask;
+	const char		*name;
+	// struct proc_dir_entry	*dir;
+} ;
+
+
+typedef enum irqreturn irqreturn_t;
+
+typedef irqreturn_t (*irq_handler_t)(int, void *);
 
 #endif // __ALINIX_KERNEL_INTERRUPT_H
---
+++ kernel/irq/manage.c
@@ -0,0 +1,27 @@
+#include <alinix/kernel.h>
+#include <alinix/interrupt.h>
+#include <alinix/memory.h>
+
+const void *free_irq(unsigned int irq, void *dev_id)
+{
+	struct irq_desc *desc = irq_to_desc(irq);
+	struct irqaction *action;
+	const char *devname;
+
+	if (!desc || irq_settings_is_per_cpu_devid(desc))
+		return NULL;
+
+#ifdef CONFIG_SMP
+	if (WARN_ON(desc->affinity_notify))
+		desc->affinity_notify = NULL;
+#endif
+
+	action = __free_irq(desc, dev_id);
+
+	if (!action)
+		return NULL;
+
+	devname = action->name;
+	free(action);
+	return devname;
+}

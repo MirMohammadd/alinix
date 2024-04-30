928354d69c0a17939759902cfeb88e469b355e3e
Heisenberg <alimirmohammad.1386@gmail.com>
2024-04-29 21:03:36 -0400

    Update

--- include/alinix/usb.h
+++ include/alinix/usb.h
@@ -85,4 +85,43 @@ struct usb_device {
 	unsigned use_generic_driver:1;
 };
 
+struct urb {
+	/* private: usb core and host controller only fields in the urb */
+	struct kref kref;		/* reference count of the URB */
+	int unlinked;			/* unlink error code */
+	void *hcpriv;			/* private data for host controller */
+	atomic_t use_count;		/* concurrent submissions counter */
+	atomic_t reject;		/* submissions will fail */
+
+	/* public: documented fields in the urb that can be used by drivers */
+	struct list_head urb_list;	/* list head for use by the urb's
+					 * current owner */
+	struct list_head anchor_list;	/* the URB may be anchored */
+	// struct usb_anchor *anchor;
+	struct usb_device *dev;		/* (in) pointer to associated device */
+	// struct usb_host_endpoint *ep;	/* (internal) pointer to endpoint */
+	unsigned int pipe;		/* (in) pipe information */
+	unsigned int stream_id;		/* (in) stream ID */
+	int status;			/* (return) non-ISO status */
+	unsigned int transfer_flags;	/* (in) URB_SHORT_NOT_OK | ...*/
+	void *transfer_buffer;		/* (in) associated data buffer */
+	u64 transfer_dma;	/* (in) dma addr for transfer_buffer */
+	// struct scatterlist *sg;		/* (in) scatter gather buffer list */
+	int num_mapped_sgs;		/* (internal) mapped sg entries */
+	int num_sgs;			/* (in) number of entries in the sg list */
+	u32 transfer_buffer_length;	/* (in) data buffer length */
+	u32 actual_length;		/* (return) actual transfer length */
+	unsigned char *setup_packet;	/* (in) setup packet (control only) */
+	u64 setup_dma;		/* (in) dma addr for setup_packet */
+	int start_frame;		/* (modify) start frame (ISO) */
+	int number_of_packets;		/* (in) number of ISO packets */
+	int interval;			/* (modify) transfer interval
+					 * (INT/ISO) */
+	int error_count;		/* (return) number of ISO errors */
+	void *context;			/* (in) context for completion */
+	// usb_complete_t complete;	/* (in) completion routine */
+	// struct usb_iso_packet_descriptor iso_frame_desc[];
+					/* (in) ISO ONLY */
+};
+
 #endif // __ALINIX_KERNEL_USB_H

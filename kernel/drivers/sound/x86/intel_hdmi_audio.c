60619692a28b11398db6e9ad225ae5e27823a6ac
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-01 08:58:37 -0400

    Updated

---
+++ kernel/drivers/sound/x86/intel_hdmi_audio.c
@@ -0,0 +1,16 @@
+#include <alinix/sound/x86/intel_hdmi_audio.h>
+#include <alinix/kernel.h>
+
+
+static const int eld_speaker_allocation_bits[] = {
+	[0] = FL | FR,
+	[1] = LFE,
+	[2] = FC,
+	[3] = RL | RR,
+	[4] = RC,
+	[5] = FLC | FRC,
+	[6] = RLC | RRC,
+	/* the following are not defined in ELD yet */
+	[7] = 0,
+};
+

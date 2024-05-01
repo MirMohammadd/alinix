142f834742d0c7e53be476f6339b99d05e540fa9
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-01 12:10:54 -0400

    Updated

--- kernel/drivers/video/video.c
+++ kernel/drivers/video/video.c
@@ -17,6 +17,7 @@
 #include <alinix/video.h>
 #include <alinix/gui/window.h>
 #include <alinix/types.h>
+#include <alinix/kernel.h>
 
 
 static int x;
@@ -34,8 +35,63 @@ void video_init(int h,int w){
     vram.width = w;
     vram.ram = (uint16_t *) 0xB8000; // Settle the RAM
 }
+void check() {
+    if(x >= vram.width) {
+        x = 0;
+        y++;
+    }
+    if(y >= vram.height) {
+        y--;
+        scroll();
+    }
+}
+
+void scroll() {
+    for(int i = 0; i < vram.width * (vram.height - 1); i++) {
+        vram.ram[i] = vram.ram[i + vram.width];
+    }
+    for(int i = 0; i < vram.width; i++) {
+        vram.ram[vram.width * (vram.height - 1) + i] = 3872;
+    }
+}
 
 void printk(char *buffer, ...){
     char str[PRINT_K_BUFFER_SIZE];
     va_list args;
+    va_start(args,buffer);
+    print(str, buffer, args);
+    va_end(args);
+    printk_string(str);
+}
+
+void printk_string(char *buffer){
+    int i = 0;
+
+    check();
+
+    while (buffer[i] != '\0'){
+        switch(buffer[i]){
+            case '\0':
+                return;
+            case '\b':
+                vram.ram[(y * vram.width) + --x] = (uint16_t)3872;
+                i++;
+                break;
+            case '\n':
+                i++;
+                y++;
+                x = 0;
+                check();
+                break;
+            
+            case '\r':
+                i++;
+                x = 0;
+                break;
+            default:
+                check();
+                vram.ram[(y * vram.width) + x++] = (uint16_t) (3840 | buffer[i++]);
+                break;
+        }
+    }
 }

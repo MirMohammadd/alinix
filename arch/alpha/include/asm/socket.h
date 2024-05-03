f26e3f10aed7237a30879f4b980fffed05db07e6
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-03 17:09:17 -0400

    Patch

--- arch/alpha/include/asm/socket.h
+++ arch/alpha/include/asm/socket.h
@@ -1,7 +1,7 @@
 #ifndef _ASM_SOCKET_H
 #define _ASM_SOCKET_H
 
-#include <uapi/asm/socket.h>
+// #include <uapi/asm/socket.h>
 
 /* O_NONBLOCK clashes with the bits used for socket types.  Therefore we
  * have to define SOCK_NONBLOCK to a different value here.

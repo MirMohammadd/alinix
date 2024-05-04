330a3f93380a4cf814133a6ca2202661abe49e6f
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-03 20:05:03 -0400

    Patch

--- kernel/core/convert.c
+++ kernel/core/convert.c
@@ -71,3 +71,38 @@ char* IntToHexString(uint8_t w)
         rc[i] = digits[(w>>j) & 0x0f];
     return rc;
 }
+
+
+char * IntToString(int n)
+{
+    static char ret[24];
+    int numChars = 0;
+    // Determine if integer is negative
+    bool isNegative = false;
+    if (n < 0)
+    {
+        n = -n;
+        isNegative = true;
+        numChars++;
+    }
+    // Count how much space we will need for the string
+    int temp = n;
+    do
+    {
+        numChars++;
+        temp /= 10;
+    } while (temp);
+
+    ret[numChars] = 0;
+    //Add the negative sign if needed
+    if (isNegative)
+        ret[0] = '-';
+    // Copy digits to string in reverse order
+    int i = numChars - 1;
+    do
+    {
+        ret[i--] = n % 10 + '0';
+        n /= 10;
+    } while (n);
+    return ret;
+}

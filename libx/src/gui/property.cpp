cdb26360db97cc763234cbcc27018f1476931b62
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-04 14:23:10 -0400

    Patch

--- libx/src/gui/property.cpp
+++
@@ -1,9 +0,0 @@
-#include <gui/property.h>
-#include <gui/widgets/control.h>
-
-using namespace LIBCactusOS;
-
-void LIBCactusOS::UpdateGUIPropertyTargetGUI(Control* target)
-{
-    if(target) target->ForcePaint();
-}

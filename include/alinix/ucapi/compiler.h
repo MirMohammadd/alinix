c050c733d7f351851d6228eb8ae4ce8809b84c8d
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-07 18:05:14 -0400

    Patch

--- include/alinix/ucapi/compiler.h
+++ include/alinix/ucapi/compiler.h
@@ -1,5 +1,11 @@
 #ifndef __ALINIX_KERNEL_DRIVERS_UCAPI_H
 #define __ALINIX_KERNEL_DRIVERS_UCAPI_H
 
+#define UACPI_ALWAYS_INLINE inline
+
+#define UACPI_PACKED(decl)  \
+    __pragma(pack(push, 1)) \
+    decl;                   \
+    __pragma(pack(pop))
 
 #endif

bdb1dd894158f5c9cd0a530c154e5eecd7bf5436
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-07 20:13:34 -0400

    Patched

--- include/alinix/ucapi/namespace.h
+++ include/alinix/ucapi/namespace.h
@@ -2,7 +2,17 @@
 #define __ALINIX_KERNEL_UAPI_NAME_SPACE_H
 
 #include "types.h"
-typedef struct uacpi_namespace_node ;
+
+struct uacpi_namespace_node;
+typedef struct uacpi_namespace_node {
+    struct uacpi_shareable shareable;
+    uacpi_object_name name;
+    u32 flags;
+    uacpi_object *object;
+    struct uacpi_namespace_node *parent;
+    struct uacpi_namespace_node *child;
+    struct uacpi_namespace_node *next;
+} uacpi_namespace_node;
 typedef struct uacpi_object {
     struct uacpi_shareable shareable;
     u8 type;

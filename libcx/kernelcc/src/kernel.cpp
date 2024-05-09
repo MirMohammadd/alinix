64260e1c964a4f77bb9a3e5036e13118dc61dc18
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-09 17:16:24 -0400

    Minor update

--- libcx/kernelcc/src/kernel.cpp
+++ libcx/kernelcc/src/kernel.cpp
@@ -54,6 +54,7 @@ SOFTWARE.
 #include <core/fpu.h>
 #include <core/power.h>
 #include <installer/installer.h>
+#include <system/reboot.hpp>
 
 using namespace CactusOS;
 using namespace CactusOS::common;

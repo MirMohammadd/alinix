a6cec5a02da46343c6b615167dbbc14091e1aaea
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-09 18:22:51 -0400

    FADT update

---
+++ include/alinix/fadt.h
@@ -0,0 +1,24 @@
+#ifndef __ALINIX_KERNEL_FADT_H
+#define __ALINIX_KERNEL_FADT_H
+
+
+/**
+ * @ref https://wiki.osdev.org/RSDT
+*/
+
+#include <alinix/types.h>
+
+
+struct ACPISDTHeader {
+  char Signature[4];
+  uint32_t Length;
+  uint8_t Revision;
+  uint8_t Checksum;
+  char OEMID[6];
+  char OEMTableID[8];
+  uint32_t OEMRevision;
+  uint32_t CreatorID;
+  uint32_t CreatorRevision;
+};
+
+#endif

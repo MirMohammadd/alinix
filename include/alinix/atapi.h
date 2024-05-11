d2d4ae45e5a9a748743b99343fe94c2f513ec7af
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-10 22:13:50 -0400

    Update

--- include/alinix/atapi.h
+++ include/alinix/atapi.h
@@ -21,5 +21,33 @@
 #define __ALINIX_KERNEL_AT_API_H
 
 
+#include <alinix/port.h>
+#include <alinix/types.h>
+
+/**
+ * @ref https://wiki.osdev.org/ATAPI
+*/
+// Handy register number defines
+#define DATA 0
+#define ERROR_R 1
+#define SECTOR_COUNT 2
+#define LBA_LOW 3
+#define LBA_MID 4
+#define LBA_HIGH 5
+#define DRIVE_SELECT 6
+#define COMMAND_REGISTER 7
+ 
+// Control register defines
+#define CONTROL 0x206
+ 
+#define ALTERNATE_STATUS 0
+
+
+static void ata_io_wait(const uint8_t p);
+
+// Reads sectors starting from lba to buffer
+int read_cdrom(uint16_t port, bool slave, uint32_t lba, uint32_t sectors, uint16_t *buffer);
+
+
 
 #endif

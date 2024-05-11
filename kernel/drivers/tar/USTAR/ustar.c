d5e9abf372640a2e42ed9d71a4a4d4113d3ed577
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-11 14:07:39 -0400

    Update shutdown.c with GNU Affero General Public License v3
    The file shutdown.c has been updated to include the GNU Affero General Public License version 3 (AGPLv3) header. The header states that the

--- kernel/drivers/tar/USTAR/ustar.c
+++ kernel/drivers/tar/USTAR/ustar.c
@@ -14,20 +14,3 @@
 **You should have received a copy of the GNU Affero General Public License
 **along with AliNix. If not, see <https://www.gnu.org/licenses/>.
 */
-#include <alinix/shutdown.h>
-#include <alinix/init.h>
-#include <alinix/kernel.h>
-
-
-/**
- * @ref https://wiki.osdev.org/Shutdown
-*/
-
-/**
- * @brief Function that administrate the shutdown proccess
- * @param None
-*/
-void shutdown(){
-    outportw(0x604, 0x2000);
-    // This is mostly used in QEMU
-}

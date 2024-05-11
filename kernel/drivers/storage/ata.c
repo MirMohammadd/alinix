167872fd84884519fa12efc5099d85d1bcb4f01e
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-10 22:17:19 -0400

    Patch

---
+++ kernel/drivers/storage/ata.c
@@ -0,0 +1,51 @@
+/**
+ * @author Ali Mirmohammad
+ * @file node.h
+ * *************************************IMPORTANT ALINIX LICENSE TERM********************************************
+ ** This file is part of AliNix.
+
+**AliNix is free software: you can redistribute it and/or modify
+**it under the terms of the GNU Affero General Public License as published by
+**the Free Software Foundation, either version 3 of the License, or
+**(at your option) any later version.
+
+**AliNix is distributed in the hope that it will be useful,
+**but WITHOUT ANY WARRANTY; without even the implied warranty of
+**MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+**GNU Affero General Public License for more details.
+
+**You should have received a copy of the GNU Affero General Public License
+**along with AliNix. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include <alinix/kernel.h>
+#include <alinix/init.h>
+#include <alinix/port.h>
+#include <alinix/atapi.h>
+
+static void ata_io_wait(const uint8_t p){
+	inportb(p + CONTROL + ALTERNATE_STATUS);
+	inportb(p + CONTROL + ALTERNATE_STATUS);
+	inportb(p + CONTROL + ALTERNATE_STATUS);
+	inportb(p + CONTROL + ALTERNATE_STATUS);
+}
+
+int read_cdrom(uint16_t port, bool slave, uint32_t lba, uint32_t sectors, uint16_t *buffer){
+            // The command
+	volatile uint8_t read_cmd[12] = {0xA8, 0,
+	                                 (lba >> 0x18) & 0xFF, (lba >> 0x10) & 0xFF, (lba >> 0x08) & 0xFF,
+	                                 (lba >> 0x00) & 0xFF,
+	                                 (sectors >> 0x18) & 0xFF, (sectors >> 0x10) & 0xFF, (sectors >> 0x08) & 0xFF,
+	                                 (sectors >> 0x00) & 0xFF,
+	                                 0, 0};
+                
+        outportb(port + DRIVE_SELECT,0xA0 & (slave << 4));
+        ata_io_wait(port);
+        outportb(port + ERROR_R,0x00);
+        outportb(port + LBA_MID, 2048 & 0xFF);
+        outportb(port + LBA_HIGH, 2048 >> 8);
+        outportb(port + COMMAND_REGISTER, 0xA0); // Packet command
+        ata_io_wait(port); // I think we might need this delay, not sure, so keep this
+
+        // Halt here for status
+}

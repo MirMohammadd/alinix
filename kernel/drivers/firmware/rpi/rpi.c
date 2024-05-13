8e4db9f4b6aee40d36bb54aea1ca92ebe2963dd2
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-12 20:34:01 -0400

    rpi functions update

--- kernel/drivers/firmware/rpi/rpi.c
+++ kernel/drivers/firmware/rpi/rpi.c
@@ -14,3 +14,25 @@
 **You should have received a copy of the GNU Affero General Public License
 **along with AliNix. If not, see <https://www.gnu.org/licenses/>.
 */
+
+#include <alinix/types.h>
+#include <alinix/kernel.h>
+#include <alinix/init.h>
+
+static uint32_t MMIO_BASE;
+
+static inline void mmio_init(int raspi){
+    switch(raspi){
+        case 2:
+        case 3:  MMIO_BASE = 0x3F000000; break; // for raspi2 & 3
+        case 4:  MMIO_BASE = 0xFE000000; break; // for raspi4
+        default: MMIO_BASE = 0x20000000; break; // for raspi1, raspi zero etc.
+    }
+}
+
+/*Memory-Mapped I/O output*/
+
+static inline void mmio_write(uint32_t reg, uint32_t data){
+    *(volatile uint32_t*)(MMIO_BASE+reg) = data;
+}
+

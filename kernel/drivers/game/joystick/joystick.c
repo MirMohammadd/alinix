5a9bdd8c82d32dc5474f79612ce90b66a052e842
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-16 19:07:23 -0400

    Update for the kernel docs

--- kernel/drivers/game/joystick/joystick.c
+++ kernel/drivers/game/joystick/joystick.c
@@ -14,6 +14,11 @@
 **You should have received a copy of the GNU Affero General Public License
 **along with AliNix. If not, see <https://www.gnu.org/licenses/>.
 */
+
+/**
+ * @abstraction:
+ *  - Provides Joystick utils for kernel.
+*/
 #include <alinix/gameport.h>
 #include <alinix/types.h>
 #include <alinix/port.h>

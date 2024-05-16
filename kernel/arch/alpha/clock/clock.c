0be8e0b6d4e5329583c3fac361bce313c00c5c8a
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-16 17:11:21 -0400

    Added missing doc

--- kernel/arch/alpha/clock/clock.c
+++ kernel/arch/alpha/clock/clock.c
@@ -14,6 +14,13 @@
 **You should have received a copy of the GNU Affero General Public License
 **along with AliNix. If not, see <https://www.gnu.org/licenses/>.
 */
+
+
+/**
+ * @abstraction:
+ *  - Provides RTC and system clock utility.
+*/
+
 #include <alinix/system.h>
 #include <alinix/init.h>
 #include <alinix/kernel.h>

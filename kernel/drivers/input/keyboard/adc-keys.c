1c2158e07ca9bdb34016dfc50e3c09d091e68a71
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-16 19:18:09 -0400

    More updates for Documents.

--- kernel/drivers/input/keyboard/adc-keys.c
+++ kernel/drivers/input/keyboard/adc-keys.c
@@ -15,6 +15,11 @@
 **along with AliNix. If not, see <https://www.gnu.org/licenses/>.
 */
 
+/**
+ * @abstraction:
+ *  - Util over the keyboard keys.
+*/
+
 #include <alinix/err.h>
 #include <alinix/types.h>
 #include <alinix/kernel.h>

4effa8fef47852e313b3787ab59d5c67a6c8a872
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-16 19:13:17 -0400

    Docs added

--- kernel/drivers/gui/window.c
+++ kernel/drivers/gui/window.c
@@ -14,6 +14,11 @@
 **You should have received a copy of the GNU Affero General Public License
 **along with AliNix. If not, see <https://www.gnu.org/licenses/>.
 */
+
+/**
+ * @abstraction:
+ *  - Provide utils over the window of the kernel GUI.
+*/
 #include <alinix/mm.h>
 #include <alinix/paging.h>
 #include <alinix/gui/window.h>

ce05e86804339110216030fcdab7839445dc3d7e
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-16 19:19:15 -0400

    Updated the docs.

--- kernel/drivers/input/input.c
+++ kernel/drivers/input/input.c
@@ -14,6 +14,11 @@
 **You should have received a copy of the GNU Affero General Public License
 **along with AliNix. If not, see <https://www.gnu.org/licenses/>.
 */
+
+/**
+ * @abstraction:
+ * 	- Provide util over the input I/O.
+*/
 #include <alinix/input.h>
 #include <alinix/ktime.h>
 #include <alinix/kernel.h>

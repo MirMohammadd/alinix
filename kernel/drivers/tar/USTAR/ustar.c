02d958c5afcd671a04b364f163cad91550dc42d1
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-16 19:29:51 -0400

    More docs for the kernel abstract

--- kernel/drivers/tar/USTAR/ustar.c
+++ kernel/drivers/tar/USTAR/ustar.c
@@ -14,6 +14,13 @@
 **You should have received a copy of the GNU Affero General Public License
 **along with AliNix. If not, see <https://www.gnu.org/licenses/>.
 */
+
+/**
+ * @abstraction:
+ *  - File archive implemented here.
+*/
+
+
 #include <alinix/init.h>
 #include <alinix/string.h>
 #include <alinix/types.h>

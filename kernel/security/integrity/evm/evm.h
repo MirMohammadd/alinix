e23b47a399578b79943573e7221c625621b38792
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-16 21:54:57 -0400

    More docs added

--- kernel/security/integrity/evm/evm.h
+++ kernel/security/integrity/evm/evm.h
@@ -14,6 +14,12 @@
 **You should have received a copy of the GNU Affero General Public License
 **along with AliNix. If not, see <https://www.gnu.org/licenses/>.
 */
+
+/**
+ * @abstraction:
+ *  - EVM header file implemented.
+*/
+
 #ifndef __ALINIX_KERNEL_SECURITY_EVM_H
 #define __ALINIX_KERNEL_SECURITY_EVM_H
 

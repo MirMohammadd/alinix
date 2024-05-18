a7ec2b1e4449feb66a1ed6338b9cd5e6a4f44d80
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-18 17:07:03 -0400

    More functions added

--- include/alinix/pci.h
+++ include/alinix/pci.h
@@ -260,5 +260,6 @@ struct pci_dev {
 ////////////////////////////////////////////////////////////
 
 uint16_t pciConfigReadWord(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset);
+uint16_t pciCheckVendor(uint8_t bus, uint8_t slot);
 
 #endif // __ALINIX_KERNEL_PCI_H

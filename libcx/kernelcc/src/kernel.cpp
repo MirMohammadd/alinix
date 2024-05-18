a6beaa1b507769d06f036ed173601f329fb39358
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-17 23:18:59 -0400

    More docs

--- libcx/kernelcc/src/kernel.cpp
+++ libcx/kernelcc/src/kernel.cpp
@@ -37,6 +37,11 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
+
+/**
+ * @abstract:
+ *  - Main kernel Entry.
+*/
 #include <multiboot/multiboot.h>
 #include <core/gdt.h>
 #include <core/tss.h>

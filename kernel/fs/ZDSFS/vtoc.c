594a9aa544e673890d3d8ca07b028e65d25a806b
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-17 21:12:01 -0400

    Update

--- kernel/fs/ZDSFS/vtoc.c
+++ kernel/fs/ZDSFS/vtoc.c
@@ -1,2 +1,12 @@
 #include <alinix/VTOC.h>
+#include <alinix/memory.h>
+
+void GetCHRFromVTOC(void *buffer){
+    uint16_t cyl,head;
+    uint8_t rec;
+    memcpy(& cyl,buffer + 15,2);
+    memcpy(&head,buffer + 17,2);
+    memcpy(&rec,buffer + 19,1);
+    RET;
+}
 

3995240c8e01624127c8d44547de8ce0069817c0
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-18 11:27:58 -0400

    Added missing docs

--- kernel/net/ipv4/tcp.c
+++ kernel/net/ipv4/tcp.c
@@ -14,6 +14,11 @@
 **You should have received a copy of the GNU Affero General Public License
 **along with AliNix. If not, see <https://www.gnu.org/licenses/>.
 */
+
+/**
+ * @abstract:
+ *  - Implements TCP connection for kernel.
+*/
 #include <net/tcp.h>
 #include <net/tcp_impl.h>
 #include <alinix/types.h>

287890abed75e63eb62976123160741397f1491a
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-18 17:24:07 -0400

    Update

--- kernel/drivers/pci/pci.c
+++ kernel/drivers/pci/pci.c
@@ -66,3 +66,15 @@ void checkAllBuses(void){
         }
     }
 }
+
+void checkBus(uint8_t bus){
+    uint8_t device;
+    for (device = 0;device < 32;device++){
+        checkDevice(bus,device);
+    }
+}
+
+uint64_t arch_msi_address(uint64_t *data, size_t vector, uint32_t processor, uint8_t edgetrigger, uint8_t deassert) {
+	*data = (vector & 0xFF) | (edgetrigger == 1 ? 0 : (1 << 15)) | (deassert == 1 ? 0 : (1 << 14));
+	return (0xFEE00000 | (processor << 12));
+}

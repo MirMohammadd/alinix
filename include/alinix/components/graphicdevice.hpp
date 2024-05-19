18fd4ad9891c8cec9def0b111aa2055cc31ec6a4
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-19 16:25:50 -0400

    More docs added.

--- include/alinix/components/graphicdevice.hpp
+++ include/alinix/components/graphicdevice.hpp
@@ -14,6 +14,11 @@
 **You should have received a copy of the GNU Affero General Public License
 **along with AliNix. If not, see <https://www.gnu.org/licenses/>.
 */
+
+/**
+ * @abstraction:
+ *  - Kernel graphic drivers for the C++ section.
+*/
 #ifndef __ALINIX_KERNEL_COMPONENTS_GRAPHIC_DEVICE_HPP
 #define __ALINIX_KERNEL_COMPONENTS_GRAPHIC_DEVICE_HPP
 

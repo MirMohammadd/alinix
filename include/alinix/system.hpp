15aae6936015f708092fff075c9ccce3e22ebaa8
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-19 19:19:00 -0400

    Added missing doc

--- include/alinix/system.hpp
+++ include/alinix/system.hpp
@@ -14,6 +14,11 @@
 **You should have received a copy of the GNU Affero General Public License
 **along with AliNix. If not, see <https://www.gnu.org/licenses/>.
 */
+
+/**
+ * @abstraction:
+ *  - Kernel header file for system settings for C++.
+*/
 #ifndef __ALINIX_KERNEL_SYSTEM_HPP
 #define __ALINIX_KERNEL_SYSTEM_HPP
 

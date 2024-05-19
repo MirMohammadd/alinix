ae38e25ce43700868329fc4da16c33854e65ec8b
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-19 19:17:38 -0400

    Added missing docs.

--- include/alinix/systeminfo.h
+++ include/alinix/systeminfo.h
@@ -14,6 +14,11 @@
 **You should have received a copy of the GNU Affero General Public License
 **along with AliNix. If not, see <https://www.gnu.org/licenses/>.
 */
+
+/**
+ * @abstraction:
+ *  - Kernel system info implemented.
+*/
 #ifndef __ALINIX_KERNEL_SYSTEM_INFO_H_
 #define __ALINIX_KERNEL_SYSTEM_INFO_H_
 

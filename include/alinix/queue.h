de48e05e415212047afb4c2914dbd37ef598466f
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-19 19:34:36 -0400

    Added missed docs

--- include/alinix/queue.h
+++ include/alinix/queue.h
@@ -17,6 +17,11 @@
 **You should have received a copy of the GNU Affero General Public License
 **along with AliNix. If not, see <https://www.gnu.org/licenses/>.
 */
+
+/**
+ * @abstract:
+ * 	- Kernel queue header file implemented for DS.
+*/
 #ifndef __ALINIX_KERNEL_QUEUE_H
 #define __ALINIX_KERNEL_QUEUE_H
 

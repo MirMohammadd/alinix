913f6dca16be217ec5b5eb50e9bf23256203e5e3
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-19 16:23:40 -0400

    Another update for the docs.

--- include/alinix/disk/disk.h
+++ include/alinix/disk/disk.h
@@ -14,6 +14,11 @@
 **You should have received a copy of the GNU Affero General Public License
 **along with AliNix. If not, see <https://www.gnu.org/licenses/>.
 */
+
+/**
+ * @abstraction:
+ *  - Kernel DISK driver implementation.
+*/
 #ifndef __ALINIX_KERNEL_DISK_H
 #define __ALINIX_KERNEL_DISK_H
 

15015381747d84a24a7ba23f0a4679399b00a4e1
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-19 16:15:55 -0400

    Update for the Docs.

--- include/alinix/sound/pcm.h
+++ include/alinix/sound/pcm.h
@@ -14,6 +14,11 @@
 **You should have received a copy of the GNU Affero General Public License
 **along with AliNix. If not, see <https://www.gnu.org/licenses/>.
 */
+
+/**
+ * @abstraction:
+ * 	- Kernel PCM sound driver header file.
+*/
 #ifndef __ALINIX_KERNEL_SOUND_PCM_H
 #define __ALINIX_KERNEL_SOUND_PCM_H
 

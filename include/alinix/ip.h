9a848bf4b61423a25898338d613fde10b9f93f66
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-19 12:39:41 -0400

    Added more missing docs

--- include/alinix/ip.h
+++ include/alinix/ip.h
@@ -14,6 +14,11 @@
 **You should have received a copy of the GNU Affero General Public License
 **along with AliNix. If not, see <https://www.gnu.org/licenses/>.
 */
+
+/**
+ * @abstraction:
+ *  - Ip Address util defined in this header file.
+*/
 #ifndef __ALINIX_KERNEL_IP_H
 #define __ALINIX_KERNEL_IP_H
 

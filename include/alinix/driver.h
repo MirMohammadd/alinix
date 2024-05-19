4d83a6af2ed3aa119640fb971fdefcd55fb14ebd
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-19 15:54:50 -0400

    New docs added.

--- include/alinix/driver.h
+++ include/alinix/driver.h
@@ -14,6 +14,11 @@
 **You should have received a copy of the GNU Affero General Public License
 **along with AliNix. If not, see <https://www.gnu.org/licenses/>.
 */
+
+/**
+ * @abstract:
+ * 	- kernel generic driver header file.
+*/
 #ifndef __ALINIX___KERNEL_DRIVER_H
 #define __ALINIX___KERNEL_DRIVER_H
 

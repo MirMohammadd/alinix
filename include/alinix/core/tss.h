abda8c766e93b0c6f5f8b0d5563fe3a440b41ab1
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-19 16:24:49 -0400

    Update for the kernel docs.

--- include/alinix/core/tss.h
+++ include/alinix/core/tss.h
@@ -17,6 +17,11 @@
 **You should have received a copy of the GNU Affero General Public License
 **along with AliNix. If not, see <https://www.gnu.org/licenses/>.
 */
+
+/**
+ * @abstraction:
+ *  - Kernel TSS implementation.
+*/
 #ifndef __ALINIX_KERNEL_CORE_TSS_H
 #define __ALINIX_KERNEL_CORE_TSS_H
 

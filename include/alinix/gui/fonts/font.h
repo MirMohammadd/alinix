32b132084806685e21844cbfc53f6939f147330b
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-19 15:27:48 -0400

    More docs added.

--- include/alinix/gui/fonts/font.h
+++ include/alinix/gui/fonts/font.h
@@ -14,6 +14,11 @@
 **You should have received a copy of the GNU Affero General Public License
 **along with AliNix. If not, see <https://www.gnu.org/licenses/>.
 */
+
+/**
+ * @abstract:
+ *  - Kernel fonts implementation.
+*/
 #ifndef __ALINIX_KERNEL_FONT_H
 #define __ALINIX_KERNEL_FONT_H
 

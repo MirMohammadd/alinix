384ca8b8b23a6ff0a5456b863cc291dae6553f1f
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-19 19:30:49 -0400

    Added missing docs.

--- include/alinix/RTL8139.h
+++ include/alinix/RTL8139.h
@@ -14,6 +14,11 @@
 **You should have received a copy of the GNU Affero General Public License
 **along with AliNix. If not, see <https://www.gnu.org/licenses/>.
 */
+
+/**
+ * @abstraction:
+ *  - Kernel RTL device driver header file implementations.
+*/
 #ifndef __ALINIX_KERNEL_RT_81_39_H  
 #define __ALINIX_KERNEL_RT_81_39_H
 

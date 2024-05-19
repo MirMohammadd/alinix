f716ff42c937b100c4d6cfb6d7f92298d677ba89
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-19 19:35:46 -0400

    Minor update for docs.

--- include/alinix/capability.h
+++ include/alinix/capability.h
@@ -14,6 +14,11 @@
 **You should have received a copy of the GNU Affero General Public License
 **along with AliNix. If not, see <https://www.gnu.org/licenses/>.
 */
+
+/**
+ * @abstract:
+ * 	- Kernel capability header file implemented.
+*/
 #ifndef __ALINIX_KERNEL_CAPABILITY_H
 #define __ALINIX_KERNEL_CAPABILITY_H
 
--- include/alinix/clk.h
+++ include/alinix/clk.h
@@ -14,6 +14,11 @@
 **You should have received a copy of the GNU Affero General Public License
 **along with AliNix. If not, see <https://www.gnu.org/licenses/>.
 */
+
+/**
+ * @abstraction:
+ * 	- Kernel CLK header file implemented.
+*/
 #ifndef __ALINIX_KERNEL_CLK_H
 #define __ALINIX_KERNEL_CLK_H
 

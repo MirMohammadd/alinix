6b26e012231fcc69a77343d72dc6698b309617be
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-19 19:26:10 -0400

    Update for the documents

--- include/alinix/skbuff.h
+++ include/alinix/skbuff.h
@@ -14,6 +14,11 @@
 **You should have received a copy of the GNU Affero General Public License
 **along with AliNix. If not, see <https://www.gnu.org/licenses/>.
 */
+
+/**
+ * @abstraction:
+ *  - Kernel Sk buffer implementation header file.
+*/
 #ifndef __ALINIX_KERNEL_SK_BUFF_H
 #define __ALINIX_KERNEL_SK_BUFF_H
 

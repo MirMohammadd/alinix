dc851894d13250d014386f1d0cdd001e9ec5ffbf
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-19 12:37:20 -0400

    Added missing document.

--- include/alinix/interrupt.h
+++ include/alinix/interrupt.h
@@ -14,6 +14,11 @@
 **You should have received a copy of the GNU Affero General Public License
 **along with AliNix. If not, see <https://www.gnu.org/licenses/>.
 */
+/**
+ * @abstraction:
+ * 	- Defines interrupts and IRQ.
+*/
+
 #ifndef __ALINIX_KERNEL_INTERRUPT_H
 #define __ALINIX_KERNEL_INTERRUPT_H
 

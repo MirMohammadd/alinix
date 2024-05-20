39ed517b8f9b3f46a16ccec7df88c8b70a9806d7
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-19 22:31:41 -0400

    Added missing docs.

--- include/alinix/vga_switcheroo.h
+++ include/alinix/vga_switcheroo.h
@@ -14,6 +14,11 @@
 **You should have received a copy of the GNU Affero General Public License
 **along with AliNix. If not, see <https://www.gnu.org/licenses/>.
 */
+
+/**
+ * @abstraction:
+ * 	- Kernel VGA drivers added.
+*/
 #ifndef __ALINIX_KERNEL_VGA_SWITCH_HEROO_H
 #define __ALINIX_KERNEL_VGA_SWITCH_HEROO_H
 

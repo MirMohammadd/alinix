e063b43f64ebb099b8ca39ea6b9e38b09c9f515e
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-19 22:11:15 -0400

    Added header file docs.

--- include/alinix/ktime.h
+++ include/alinix/ktime.h
@@ -14,6 +14,11 @@
 **You should have received a copy of the GNU Affero General Public License
 **along with AliNix. If not, see <https://www.gnu.org/licenses/>.
 */
+
+/**
+ * @abstraction:
+ * 	- Kernel time header file implemented.
+*/
 #ifndef __ALINIX_KERNEL_K_TIME_H
 #define __ALINIX_KERNEL_K_TIME_H
 

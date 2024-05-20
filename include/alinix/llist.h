0bab665f980523589d71c18be443b15723b5f068
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-19 22:14:00 -0400

    Docs added.

--- include/alinix/llist.h
+++ include/alinix/llist.h
@@ -14,6 +14,11 @@
 **You should have received a copy of the GNU Affero General Public License
 **along with AliNix. If not, see <https://www.gnu.org/licenses/>.
 */
+
+/**
+ * @abstraction:
+ * 	- Kernel llist header file and macros implemented.
+*/
 #ifndef __ALINIX_KERNEL_L_LIST_H
 #define __ALINIX_KERNEL_L_LIST_H
 

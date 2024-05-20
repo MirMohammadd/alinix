c81ddaa23992fd99528f0f5b14b5e40246142228
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-19 22:22:42 -0400

    Added more docs for kernel

--- include/alinix/panic.h
+++ include/alinix/panic.h
@@ -17,6 +17,11 @@
 **You should have received a copy of the GNU Affero General Public License
 **along with AliNix. If not, see <https://www.gnu.org/licenses/>.
 */
+
+/**
+ * @abstraction:
+ *  - Kernel panic, useful when kernel panics and encounter errors.
+*/
 #ifndef __ALINIX_KERNEL_PANIC_H
 #define __ALINIX_KERNEL_PANIC_H
 

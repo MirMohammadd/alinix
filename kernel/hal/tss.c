d7624967d6c126e0c219088ac2e7743d2f35b75d
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-19 22:57:36 -0400

    Kernel docs added.

--- kernel/hal/tss.c
+++ kernel/hal/tss.c
@@ -14,6 +14,11 @@
 **You should have received a copy of the GNU Affero General Public License
 **along with AliNix. If not, see <https://www.gnu.org/licenses/>.
 */
+
+/**
+ * @abstraction:
+ *  - Kernel TSS implementations.
+*/
 void flush_tss() {
     asm volatile("mov $0x2B, %ax; \
                   ltr %ax;");

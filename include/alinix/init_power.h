bdfea5d11544b9cf90d5cdf3b1ef29dc7719a1e3
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-19 22:07:27 -0400

    Updated the kernel docs.

--- include/alinix/init_power.h
+++ include/alinix/init_power.h
@@ -14,6 +14,11 @@
 **You should have received a copy of the GNU Affero General Public License
 **along with AliNix. If not, see <https://www.gnu.org/licenses/>.
 */
+
+/**
+ * @abstraction:
+ *  - Starts the kernel power manager.
+*/
 #ifndef __ALINIX_KERNEL_INIT_POWER_H
 #define __ALINIX_KERNEL_INIT_POWER_H
 

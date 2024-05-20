1825e9131db6bd7e7b607fef3822d2c90e60bc47
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-20 15:42:31 -0400

    Added document for fpu_load_control_word

--- include/alinix/fpu.h
+++ include/alinix/fpu.h
@@ -42,6 +42,10 @@ struct FPUControlWord
 
 static void Enable();
 
+/**
+ * @brief
+ *  - Used to load a new value to FPU.
+*/
 inline void fpu_load_control_word(const uint16_t control){
     asm volatile("fldcw %0;"::"m"(control)); 
 }

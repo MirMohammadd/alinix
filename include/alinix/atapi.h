0d75eda9aa952a276d6ba3e653b49ca2e4bd6a2c
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-19 20:47:11 -0400

    More docs.

--- include/alinix/atapi.h
+++ include/alinix/atapi.h
@@ -17,6 +17,11 @@
 **You should have received a copy of the GNU Affero General Public License
 **along with AliNix. If not, see <https://www.gnu.org/licenses/>.
 */
+
+/**
+ * @abstraction:
+ *  - Kernel API.
+*/
 #ifndef __ALINIX_KERNEL_AT_API_H
 #define __ALINIX_KERNEL_AT_API_H
 

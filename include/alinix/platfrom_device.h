ef00661c829fde06974f815fae1cb71a0b8cd754
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-19 22:26:09 -0400

    Added kernel missing docs.

--- include/alinix/platfrom_device.h
+++ include/alinix/platfrom_device.h
@@ -14,6 +14,11 @@
 **You should have received a copy of the GNU Affero General Public License
 **along with AliNix. If not, see <https://www.gnu.org/licenses/>.
 */
+
+/**
+ * @abstraction:
+ * 	- Kernel platform device header file implementation.
+*/
 #ifndef __PLATFORM_DEVICE_H
 #define __PLATFORM_DEVICE_H
 

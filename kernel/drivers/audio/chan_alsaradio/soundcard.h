1315438ee3d8f7def8392bf558d4cac5e885e58a
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-19 23:09:07 -0400

    Kernel docs added.

--- kernel/drivers/audio/chan_alsaradio/soundcard.h
+++ kernel/drivers/audio/chan_alsaradio/soundcard.h
@@ -14,6 +14,11 @@
 **You should have received a copy of the GNU Affero General Public License
 **along with AliNix. If not, see <https://www.gnu.org/licenses/>.
 */
+
+/**
+ * @abstraction:
+ * 	- Kernel sound card header file.
+*/
 #ifndef __ALINIX_KERNEL_SOUND_CARD_AUDIO_H
 #define __ALINIX_KERNEL_SOUND_CARD_AUDIO_H
 

cf11ae3c59815e700bd491860114ee8898344a3f
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-19 22:30:02 -0400

    Kernel docs added.

--- include/alinix/print.h
+++ include/alinix/print.h
@@ -17,6 +17,11 @@
 **You should have received a copy of the GNU Affero General Public License
 **along with AliNix. If not, see <https://www.gnu.org/licenses/>.
 */
+
+/**
+ * @abstraction:
+ *  - Kernel print functions header file implemented.
+*/
 #ifndef __ALINIX_KERNEL_PRINT_NORM_H
 #define __ALINIX_KERNEL_PRINT_NORM_H
 

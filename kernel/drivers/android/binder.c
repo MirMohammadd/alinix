302b1ebb2daadbf5a9e647c1b2f242354d8992a1
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-19 23:09:49 -0400

    Kernel stuff added for docs.

--- kernel/drivers/android/binder.c
+++ kernel/drivers/android/binder.c
@@ -14,6 +14,11 @@
 **You should have received a copy of the GNU Affero General Public License
 **along with AliNix. If not, see <https://www.gnu.org/licenses/>.
 */
+
+/**
+ * @abstraction:
+ * 	- Kernel stuff binder for android.
+*/
 #include <alinix/types.h>
 #include <alinix/kernel.h>
 

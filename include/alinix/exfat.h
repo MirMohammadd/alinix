6aa2b6ea3cce3b006ebee4a000d11f7e24e3b0f7
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-20 20:18:17 -0400

    FS updates

--- include/alinix/exfat.h
+++ include/alinix/exfat.h
@@ -19,3 +19,26 @@
  * @abstraction:
  *  - Provides util for extended fat file system in header file.
 */
+
+#include <alinix/types.h>
+
+struct bootsector {
+	char jump[3];
+	char fsid[8];
+	char pad[53];
+	uint64_t unk; // sectors before first cluster too?
+	uint64_t fsSizeInSectors;
+        uint32_t sectorsToStartOfFat;
+        uint32_t sectorsUsedForFat;
+        uint32_t sectorStartOfFirstCluster;
+        uint32_t clusterCountInFs;
+        uint32_t clusterForRootDirectory;
+        uint32_t unk[2];
+        uint8_t logBytesPerSector, logSectorsPerCluster, fatCount, driveId; // 2-log for both. bytesPerCluster = 1 << (logBytesPerSector + logSectorsPerCluster).
+        uint8_t percentInUse; // for display ala windows computer explorer
+        uint8_t unk[7];
+        char bootcode[390];
+        char bootsign[2];
+};
+
+uint32_t BootChecksum(unsigned char * Sectors,unsigned short BytesPerSector);

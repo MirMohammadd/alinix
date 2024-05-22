2faeb92e1559772748e92a2bc93532b3434f1efb
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-22 17:36:27 -0400

    Update and stuff

--- kernel/drivers/sound/x86/als4000/als4000.c
+++ kernel/drivers/sound/x86/als4000/als4000.c
@@ -13,8 +13,7 @@ uint32_t dev_mixer_read(uint32_t *base, uint32_t reg){
     return res;
 }
 
-uint32_t dev_mixer_write(uint32_t *base, uint32_t reg){
+void dev_mixer_write(uint32_t *base, uint32_t reg, uint32_t val){
     uint32_t res;
     res = inportl(base[0] + REG_SB_BASE); // Use inportl instead of outportb
-    return res;
 }

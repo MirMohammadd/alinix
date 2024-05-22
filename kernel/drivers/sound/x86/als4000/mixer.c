6dc25665bc21b672230dc09905b8e4ac994d76e5
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-22 17:12:54 -0400

    Some updates

--- kernel/drivers/sound/x86/als4000/mixer.c
+++ kernel/drivers/sound/x86/als4000/mixer.c
@@ -42,5 +42,25 @@ int get_set_volume(uint8_t *base,struct volume_level *level,int flag){
 			return 22;
 		default:
 			return 22;
+    } if (flag){
+        if (level->right < 0)
+            level->right = 0;
+        else if (level->right > max_level)
+            level->right = max_level;
+        if (level->left < 0)
+            level->left = 0;
+        else if (level->left > max_level)
+            level->left = max_level;
+        
+        dev_mixer_write(base,cmdLeft,0x1f - level->left);
+        dev_mixer_write(base,cmdRight,0x1f - level->right);
+        mixer_value[cmdLeft] = 0x1f - level->left;
+        mixer_value[cmdRight] = 0x1f - level->right;
+    }else {
+        dev_mixer_read(base,cmdLeft);
+        dev_mixer_read(base,cmdRight);
+        level->left = 0x1f - mixer_value[cmdLeft];
+        level->right = 0x1f - mixer_value[cmdRight];
     }
+    return 0;
 }

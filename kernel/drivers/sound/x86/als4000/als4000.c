2c7317733827e8b56b7376a8c2999f3a95203275
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-22 17:34:24 -0400

    Some updates

---
+++ kernel/drivers/sound/x86/als4000/als4000.c
@@ -0,0 +1,14 @@
+#include <alinix/drivers/audio/als4000/als4000.h>
+#include <alinix/drivers/audio/als4000/sound.h>
+#include <alinix/drivers/audio/als4000/mixer.h>
+
+
+#include <alinix/module.h>
+#include <alinix/kernel.h>
+#include <alinix/init.h>
+
+uint32_t dev_mixer_read(uint32_t *base, uint32_t reg){
+    uint32_t res;
+    res = inportl(base[0] + REG_SB_BASE); // Use inportl instead of outportb
+    return res;
+}

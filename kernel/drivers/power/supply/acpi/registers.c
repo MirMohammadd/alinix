ae77d9f978de95d5ad3331fefe9067822a995b1c
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-24 22:12:41 -0400

    Some updates for the kernel docs.

--- kernel/drivers/power/supply/acpi/registers.c
+++ kernel/drivers/power/supply/acpi/registers.c
@@ -24,6 +24,11 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
+
+/**
+ * @brief ACPI driver for kernel implemented.
+ * Kernel registers.
+*/
 #include <alinix/ucapi/registers.h>
 #include <alinix/ucapi/acpi.h>
 #include <alinix/ucapi/context.h>

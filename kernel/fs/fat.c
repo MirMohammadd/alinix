c563737a64d2dc5da22322e83bf7b57fd681219a
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-25 21:03:33 -0400

    Module headers

--- kernel/fs/fat.c
+++ kernel/fs/fat.c
@@ -26,6 +26,7 @@
 #include <alinix/fs/fat.h>
 #include <alinix/memory.h>
 #include <alinix/types.h>
+#include <alinix/module.h>
 
 Disk* Disk_create(uint32_t controllerIndex, void* controller, DiskType type, uint64_t size, uint32_t blocks, uint32_t blocksize){
     /**

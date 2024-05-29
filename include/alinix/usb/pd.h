b292d10c40446c7fc79883a5fb14ffecbd04853b
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-29 17:55:16 -0400

    Added missing Doc.

--- include/alinix/usb/pd.h
+++ include/alinix/usb/pd.h
@@ -14,6 +14,11 @@
 **You should have received a copy of the GNU Affero General Public License
 **along with AliNix. If not, see <https://www.gnu.org/licenses/>.
 */
+
+/**
+ * @abstraction:
+ * 	- Kernel USB header file util.
+*/
 #ifndef __ALINIX_KERNEL_USB_PD_H
 #define __ALINIX_KERNEL_USB_PD_H
 

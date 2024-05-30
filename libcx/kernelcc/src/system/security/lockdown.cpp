e45b2d69e6cfd69b0d721edb47339243cc15c916
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-29 22:45:35 -0400

    Added missing Docs.

--- libcx/kernelcc/src/system/security/lockdown.cpp
+++ libcx/kernelcc/src/system/security/lockdown.cpp
@@ -36,6 +36,8 @@ using namespace CactusOS::system;
 MODULE_AUTHOR("Ali Mirmohammad")
 MODULE_DESCRIPTION("Kernel lock down implementation for security stuff")
 MODULE_LICENSE("AGPL-3.0")
+MODULE_VERSION("0.1")
+
 
 
 static const enum lockdown_reason lockdown_levels[] = {LOCKDOWN_NONE,

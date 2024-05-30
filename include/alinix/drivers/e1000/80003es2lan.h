2293762004153581304fa539cb6cd6373cd7abde
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-29 22:00:20 -0400

    Added missing Doc.

--- include/alinix/drivers/e1000/80003es2lan.h
+++ include/alinix/drivers/e1000/80003es2lan.h
@@ -1,4 +1,5 @@
 /**
+ * @file 80003es2lan
  ** This file is part of AliNix.
 
 **AliNix is free software: you can redistribute it and/or modify
--- include/alinix/drivers/e1000/manage.h
+++ include/alinix/drivers/e1000/manage.h
@@ -1,4 +1,5 @@
 /**
+ * @file nvm.h
  ** This file is part of AliNix.
 
 **AliNix is free software: you can redistribute it and/or modify

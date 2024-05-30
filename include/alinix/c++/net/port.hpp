70ccd6e8733d13f597606add617821edfecd0ea2
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-29 20:41:31 -0400

    Added some docs.

--- include/alinix/c++/net/port.hpp
+++ include/alinix/c++/net/port.hpp
@@ -1,4 +1,5 @@
 /**
+ * @file port.hpp
  ** This file is part of AliNix.
 
 **AliNix is free software: you can redistribute it and/or modify
--- include/alinix/drivers/audio/als4000/sound.h
+++ include/alinix/drivers/audio/als4000/sound.h
@@ -1,4 +1,5 @@
 /**
+ * @file sound.h
  ** This file is part of AliNix.
 
 **AliNix is free software: you can redistribute it and/or modify

64399b7aa6af31e4051025f9655e4fde7b59f2ad
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-29 22:20:04 -0400

    Added missing Doc.

--- kernel/core/bootconsole.c
+++ kernel/core/bootconsole.c
@@ -1,4 +1,5 @@
 /**
+ * @file bootconsole.c
  ** This file is part of AliNix.
 
 **AliNix is free software: you can redistribute it and/or modify
--- kernel/core/convert.c
+++ kernel/core/convert.c
@@ -1,4 +1,5 @@
 /**
+ * @file convert.c
  ** This file is part of AliNix.
 
 **AliNix is free software: you can redistribute it and/or modify

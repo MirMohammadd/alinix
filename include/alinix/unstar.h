6fcd7b84fd9638530c9524434224475dadc6777d
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-30 19:36:56 -0400

    Added missing Doc.

--- include/alinix/unstar.h
+++ include/alinix/unstar.h
@@ -31,6 +31,9 @@ int oct2bin(unsigned char *str, int size);
 /*Returns the size and the pointer to the file data*/
 int tar_lookup(unsigned char *archive, char *filename, char **out);
 
+/**
+ * @brief Getting TAR file name.
+*/
 char* getTarFileName(unsigned char* archive);
 #define TAR_FILENAME_OFFSET 0
 #define TAR_FILENAME_SIZE   100

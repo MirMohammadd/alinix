a6ab228466036793990f971fbd763d2e817c2816
Heisenberg <alimirmohammad.1386@gmail.com>
2024-05-31 21:32:57 -0400

    Added missing Doc.

--- kernel/drivers/usb/core/message.c
+++ kernel/drivers/usb/core/message.c
@@ -1,4 +1,5 @@
 /**
+ * @author Ali Mirmohammad
  * @file message.c
  ** This file is part of AliNix.
 
--- kernel/drivers/video/video.c
+++ kernel/drivers/video/video.c
@@ -1,4 +1,5 @@
 /**
+ * @author Ali Mirmohammad
  * @file video.c
  ** This file is part of AliNix.
 

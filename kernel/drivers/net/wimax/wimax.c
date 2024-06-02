4487a4efdab5607c66be5a8e0e2684e0e78fe8f6
Heisenberg <alimirmohammad.1386@gmail.com>
2024-06-01 21:01:01 -0400

    ADded extra document.

--- kernel/drivers/net/wimax/wimax.c
+++ kernel/drivers/net/wimax/wimax.c
@@ -33,6 +33,33 @@ MODULE_DESCRIPTION("Kernel WIMAX driver for networking.")
 MODULE_LICENSE("AGPL")
 MODULE_VERSION("0.1")
 
+/**
+ * Updates the radio state of a WiMAX device based on the provided RF state.
+ *
+ * @param dev A pointer to the WiMAX device structure.
+ * @param state The desired RF state of the WiMAX device.
+ *
+ * @return None.
+ *
+ * @throws None.
+ *
+ * @details
+ * This function updates the radio state of a WiMAX device based on the provided RF state.
+ * It assumes that the `dev` parameter is a valid pointer to a WiMAX device structure.
+ *
+ * The function checks the value of the `state` parameter and updates the `dev->state`
+ * member accordingly. If the `state` parameter is `WIMAX_RF_ON`, the `dev->state` member
+ * is set to `WIMAX_ST_RADIO_OFF`. If the `state` parameter is `WIMAX_RF_OFF`, the `dev->state`
+ * member is also set to `WIMAX_ST_RADIO_OFF`.
+ *
+ * If the `state` parameter is neither `WIMAX_RF_ON` nor `WIMAX_RF_OFF`, the function does
+ * nothing.
+ *
+ * @note
+ * This function assumes that the `WIMAX_RF_ON` and `WIMAX_RF_OFF` constants are defined and
+ * represent the possible RF states of the WiMAX device. It also assumes that the `WIMAX_ST_RADIO_OFF`
+ * constant is defined and represents the radio state when the WiMAX device is turned off.
+ */
 void wimax_rfkill(struct wimax_device *dev, enum wimax_rf_state state){
     if (state == WIMAX_RF_ON){
         dev->state = WIMAX_ST_RADIO_OFF;

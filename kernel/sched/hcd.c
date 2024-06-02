0fcecd56d0003af9e8c5c84007a56f634aa65f87
Heisenberg <alimirmohammad.1386@gmail.com>
2024-06-02 19:28:02 -0400

    Added extra docs.

--- kernel/sched/hcd.c
+++ kernel/sched/hcd.c
@@ -37,7 +37,31 @@ MODULE_VERSION("0.1")
 static inline hcd_urb_unlink_lock(){};
 
 
-
+/**
+ * @brief Unlinks a URB (USB Request Block) from the USB host controller.
+ *
+ * This function unlinks a URB (USB Request Block) from the USB host controller.
+ * It sets the status of the URB to the provided status value and notifies the
+ * USB host controller driver to unlink the URB.
+ *
+ * @param urb A pointer to the URB to be unlinked.
+ * @param status An integer value representing the status to be set on the URB.
+ *
+ * @return An integer value representing the result of the unlink operation.
+ *         If the unlink operation is successful, the returned value is zero.
+ *         Otherwise, a negative error code is returned.
+ *
+ * @note The function assumes that the URB is associated with a USB device.
+ *
+ * @example
+ * struct urb *urb = ...; // Example URB
+ * int status = -ECONNRESET; // Example status value
+ *
+ * int retval = usb_hcd_unlink_urb(urb, status);
+ * if (retval < 0) {
+ *     // Handle error
+ * }
+ */
 int usb_hcd_unlink_urb (struct urb *urb, int status){
     // struct usb_hcd		*hcd;
 	struct usb_device	*udev = urb->dev;

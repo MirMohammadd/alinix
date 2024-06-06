e6bd706b4983829bf6ee54b4608129e110122820
Heisenberg <alimirmohammad.1386@gmail.com>
2024-06-05 22:44:28 -0400

    Docs adde.d

--- kernel/drivers/mm/kheap.c
+++ kernel/drivers/mm/kheap.c
@@ -50,6 +50,21 @@ void kheap_init() {
     heap_info.first_header->next = NULL;
 }
 
+/**
+ * Allocates a block of memory of the specified length.
+ *
+ * This function allocates a block of memory of the specified length by calling the `first_free`
+ * function. The `first_free` function searches for the first block of free memory that is large
+ * enough to hold the requested length, and returns a pointer to that block. The `kmalloc` function
+ * simply calls `first_free` and returns the resulting pointer.
+ *
+ * @param len The length of the memory block to allocate.
+ *
+ * @return A pointer to the allocated memory block, or NULL if there is not enough free memory
+ *         to satisfy the request.
+ *
+ * @throws None
+ */
 void *kmalloc(size_t len) {
     return first_free(len);
 }
@@ -71,6 +86,19 @@ void kfree(void *ptr) {
     }
 }
 
+/**
+ * Frees a block of memory previously allocated by `kmalloc`.
+ *
+ * This function frees a block of memory that was previously allocated by `kmalloc`. It takes a
+ * pointer to the memory block to free, and searches for the corresponding heap header that was
+ * placed before the block. If the header is valid and the block is not already free, the function
+ * marks the block as free in the heap header, updates the heap information, and merges any
+ * contiguous free sections with the freed block.
+ *
+ * @param ptr A pointer to the memory block to free.
+ *
+ * @throws None
+ */
 void *first_free(size_t len) {
     heap_header_t *head = (heap_header_t *) heap_info.first_header;
     
@@ -95,14 +123,49 @@ void *first_free(size_t len) {
     return NULL;
 }
 
+/**
+ * Returns the size of the heap.
+ *
+ * This function returns the size of the heap, which is the total amount of memory that is
+ * available for allocation by the `kmalloc` function. The size is stored in the `heap_info`
+ * structure, and is accessed by this function.
+ *
+ * @return The size of the heap, in bytes.
+ *
+ * @throws None
+ */
 int get_heap_size() {
     return heap_info.size;
 }
 
+/**
+ * Returns the amount of memory currently in use by the heap.
+ *
+ * This function returns the amount of memory that is currently in use by the heap, which is the
+ * total amount of memory that has been allocated by the `kmalloc` function and has not been freed
+ * by the `kfree` function. The amount of used heap memory is stored in the `heap_info` structure,
+ * and is accessed by this function.
+ *
+ * @return The amount of memory currently in use by the heap, in bytes.
+ *
+ * @throws None
+ */
 int get_used_heap() {
     return heap_info.used;
 }
 
+/**
+ * Prints the information stored in a heap header.
+ *
+ * This function prints the information stored in a heap header, which includes the size of the
+ * block, whether the block is free or not, and the address of the next block in the heap. The
+ * information is printed using the `printk` function, which is a simple printf-like function for
+ * printing to the console.
+ *
+ * @param head A pointer to the heap header to print.
+ *
+ * @throws None
+ */
 void print_header(heap_header_t *head) {
     printk("Size: %d Is free: %d Next: %x\n", head->size, head->is_free, head->next);
 }

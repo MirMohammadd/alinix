31dd03ab78d6ba7e0fa8a1608a61175f21b867c4
Heisenberg <alimirmohammad.1386@gmail.com>
2024-06-08 22:32:54 -0400

    Docs added.

--- include/alinix/asm/bitops.h
+++ include/alinix/asm/bitops.h
@@ -36,6 +36,17 @@
 #define BIT_MASK(nr) (1UL << ((nr) % __BITS_PER_LONG))
 #define BIT_WORD(nr) ((nr) / __BITS_PER_LONG)
 
+
+/**
+ * set_bit - Set a bit in memory
+ * @nr: the bit to set
+ * @addr: the address of the memory location to change
+ *
+ * Set the bit at position @nr in the memory pointed to by @addr. This function
+ * uses inline assembly to perform an atomic bit set operation. It reads the
+ * current value from memory, sets the specified bit, and writes the new value
+ * back to memory atomically.
+ */
 static inline void
 set_bit(unsigned long nr, volatile void * addr)
 {
@@ -54,6 +65,16 @@ set_bit(unsigned long nr, volatile void * addr)
 	:"Ir" (1UL << (nr & 31)), "m" (*m));
 }
 
+
+/**
+ * test_bit - Determine if a bit is set
+ * @nr: the bit to test
+ * @addr: the address of the memory location to check
+ *
+ * Test the value of the bit at position @nr in the memory pointed to by @addr.
+ * This function uses bitwise operations to extract the value of the specified
+ * bit and returns true if the bit is set (1), or false if the bit is not set (0).
+ */
 static inline bool test_bit(unsigned int nr, unsigned long *addr)
 {
 	return 1UL & (addr[BIT_WORD(nr)] >> (nr & (BITS_PER_LONG - 1)));
